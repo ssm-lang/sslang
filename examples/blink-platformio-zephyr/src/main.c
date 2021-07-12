3b8789728256470001be6ccc29199199269c2836
sedwards-lab <sedwards@cs.columbia.edu>
2021-07-12 11:20:18 -0400

    Trying to use it as a platformio library

---
+++ examples/blink-platformio-zephyr/src/main.c
@@ -0,0 +1,9 @@
+#include <zephyr.h>
+#include <ssm.h>
+
+void main()
+{
+  printk("Sleeping for a second for you to start a terminal\r\n");
+  k_sleep(K_SECONDS(1));
+  printk("Starting...\r\n");
+}
