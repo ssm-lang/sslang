7a65924828a19fe8d74ff25e9014e0fdb3465d7f
Hans Montero <hjm2133@columbia.edu>
2021-10-05 23:34:36 -0400

    Run hlint over Passes.hs

--- src/Passes.hs
+++ src/Passes.hs
@@ -3,23 +3,22 @@ module Passes where
 import qualified Ast as A
 
 checkRoutineSignatures :: A.Program -> Bool 
-checkRoutineSignatures (A.Program decls) = and $ map checkAnnotations decls
+checkRoutineSignatures (A.Program decls) = all checkAnnotations decls
   where
     checkAnnotations (A.Function _ binds _ fnTyAnn) =
         case fnTyAnn of 
-          A.ReturnType _ -> foldl annotatedOnce True binds
-          A.CurriedType ty -> countParams ty == length binds && foldl notAnnotated True binds
-
-    annotatedOnce ret (A.Bind _ (Just _)) = ret && True
-    annotatedOnce ret (A.TupBind bds tupTy) = ret && case tupTy of
-                                                     Just _ -> foldl notAnnotated True bds
-                                                     Nothing -> foldl annotatedOnce True bds
-    annotatedOnce _ _ = False
-
-    notAnnotated ret (A.Bind _ Nothing) = ret && True
-    notAnnotated ret (A.TupBind bds Nothing) = ret && foldl notAnnotated True bds
-    notAnnotated _ _ = False
+          A.ReturnType _ -> all annotatedOnce binds
+          A.CurriedType ty -> countParams ty == length binds && all notAnnotated binds
 
     countParams (A.TArrow _ t2) = 1 + countParams t2
     countParams _ = 0
 
+    annotatedOnce (A.Bind _ (Just _)) = True
+    annotatedOnce (A.TupBind bds tupTy) = case tupTy of
+                                            Just _ -> all notAnnotated bds
+                                            Nothing -> all annotatedOnce bds
+    annotatedOnce _ = False
+
+    notAnnotated (A.Bind _ Nothing) = True
+    notAnnotated (A.TupBind bds Nothing) = all notAnnotated bds
+    notAnnotated _ = False
