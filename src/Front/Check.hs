7ace10f8d7436036dce07dc5b7e021681e34d602
Hans Montero <hjm2133@columbia.edu>
2021-10-06 16:37:41 -0400

    Flatten case-ofs in Check.hs

--- src/Front/Check.hs
+++ src/Front/Check.hs
@@ -5,18 +5,16 @@ import qualified Front.Ast as A
 checkRoutineSignatures :: A.Program -> Bool 
 checkRoutineSignatures (A.Program decls) = all checkAnnotations decls
   where
-    checkAnnotations (A.Function _ binds _ fnTyAnn) =
-        case fnTyAnn of 
-          A.ReturnType _ -> all annotatedOnce binds
-          A.CurriedType ty -> countParams ty == length binds && all notAnnotated binds
+    checkAnnotations (A.Function _ binds _ (A.ReturnType _)) = all annotatedOnce binds
+    checkAnnotations (A.Function _ binds _ (A.CurriedType ty)) =
+        countParams ty == length binds && all notAnnotated binds
 
     countParams (A.TArrow _ t2) = 1 + countParams t2
     countParams _ = 0
 
     annotatedOnce (A.Bind _ (Just _)) = True
-    annotatedOnce (A.TupBind bds tupTy) = case tupTy of
-                                            Just _ -> all notAnnotated bds
-                                            Nothing -> all annotatedOnce bds
+    annotatedOnce (A.TupBind bds (Just _)) = all notAnnotated bds
+    annotatedOnce (A.TupBind bds Nothing) = all annotatedOnce bds
     annotatedOnce _ = False
 
     notAnnotated (A.Bind _ Nothing) = True
