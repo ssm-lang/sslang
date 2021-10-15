be08824c2703b8d7f6d29517a68ca6f08d91b2a0
Hans Montero <hjm2133@columbia.edu>
2021-10-15 00:53:06 -0400

    Complete (almost) the rest of the let scanner tests

--- test/scanner/Tests/ScanLetSpec.hs
+++ test/scanner/Tests/ScanLetSpec.hs
@@ -27,10 +27,34 @@ spec = do
     scanTokenTypes input `shouldBe` Right output
 
   it "scans sequenced let definitions" $ do
-    pendingWith "help needed"
-    -- let a = a_def
-    -- let b = b_def
-    -- c
+    let input = unlines
+          [ "let a = a_def"
+          , "let b = b_def"
+          , "c"
+          ]
+        output =
+          [ TLet
+          , TLbrace
+          , TId "a"
+          , TEq
+          , TLbrace
+          , TId "a_def"
+          , TRbrace
+          , TRbrace
+          , TSemicolon
+          , TLet
+          , TLbrace
+          , TId "b"
+          , TEq
+          , TLbrace
+          , TId "b_def"
+          , TRbrace
+          , TRbrace
+          , TSemicolon
+          , TId "c"
+          ]
+    scanTokenTypes input `shouldBe` Right output
+
 
   it "scans nested let definitions" $ do
     let input = "let a = let b = c; d"
@@ -56,12 +80,30 @@ spec = do
     scanTokenTypes input `shouldBe` Right output
 
   it "scans block let definitions" $ do
-    pendingWith "help needed"
-    -- let a = a_block_1_1
-    --           a_block_1_2
-    --         a_block_2_1
-    --           a_block_2_2
-    -- outside_let
+    let input = unlines
+          [ "let a = a_block_1_1"
+          , "          a_block_1_2"
+          , "        a_block_2_1"
+          , "          a_block_2_2"
+          , "outside_let"
+          ]
+        output =
+          [ TLet
+          , TLbrace
+          , TId "a"
+          , TEq
+          , TLbrace
+          , TId "a_block_1_1"
+          , TId "a_block_1_2"
+          , TSemicolon
+          , TId "a_block_2_1"
+          , TId "a_block_2_2"
+          , TRbrace
+          , TRbrace
+          , TSemicolon
+          , TId "outside_let"
+          ]
+    scanTokenTypes input `shouldBe` Right output
 
   it "scans grouped let-bindings" $ do
     let input = unlines
@@ -95,11 +137,32 @@ spec = do
     scanTokenTypes input `shouldBe` Right output
 
   it "supports explicit braces around definition" $ do
-    pendingWith "help needed"
-    -- let a = { a_block_1_1
-    --  a_block_1_2 ;
-    --      a_block_2_1
-    --        a_block_2_2 }
+    let input = unlines
+          [ "let a = { a_block_1_1"
+          , "            a_block_1_2;"
+          , "          a_block_2_1"
+          , "            a_block_2_2 }"
+          , "outside_let"
+          ]
+        output =
+          [ TLet
+          , TLbrace
+          , TId "a"
+          , TEq
+          , TLbrace
+          , TLbrace -- Inserts extra brace pair?
+          , TId "a_block_1_1"
+          , TId "a_block_1_2"
+          , TSemicolon
+          , TId "a_block_2_1"
+          , TId "a_block_2_2"
+          , TRbrace
+          , TRbrace
+          , TRbrace
+          , TSemicolon
+          , TId "outside_let"
+          ]
+    scanTokenTypes input `shouldBe` Right output
 
   it "supports explicit braces around bind group" $ do
     pendingWith "help needed"
@@ -107,20 +170,88 @@ spec = do
     --   and b = a }
     --
     -- though this looks rather unsightly
+    -- TODO(hans): The scanner isn't actually OK with this
 
   it "supports parenthesized definition in block" $ do
-    pendingWith "help needed"
-    -- let a = (a
-    -- b c)
-    --         d
+    let input = unlines
+          [ "let a = (a"
+          , "b c)"
+          , "        d"
+          ]
+        output =
+          [ TLet
+          , TLbrace
+          , TId "a"
+          , TEq
+          , TLbrace
+          , TLparen
+          , TId "a"
+          , TId "b"
+          , TId "c"
+          , TRparen
+          , TSemicolon
+          , TId "d"
+          , TRbrace
+          , TRbrace
+          ]
+    scanTokenTypes input `shouldBe` Right output
 
   it "supports nested let-block" $ do
-    pendingWith "help needed"
-    -- let a = let b = c
-    --             c = b
-    --         d
+    let input = unlines
+          [ "let a = let b = c"
+          , "            c = b"
+          , "        d"
+          ]
+        output =
+          [ TLet
+          , TLbrace
+          , TId "a"
+          , TEq
+          , TLbrace
+          , TLet
+          , TLbrace
+          , TId "b"
+          , TEq
+          , TLbrace
+          , TId "c"
+          , TRbrace
+          , TAnd
+          , TId "c"
+          , TEq
+          , TLbrace
+          , TId "b"
+          , TRbrace
+          , TRbrace
+          , TSemicolon
+          , TId "d"
+          , TRbrace
+          , TRbrace
+          ]
+    scanTokenTypes input `shouldBe` Right output
 
   it "supports let-bound function definitions" $ do
-    pendingWith "help needed"
-    -- let f = let g x = 1
-    --         g
+    let input = unlines
+          [ "let f = let g x = 1"
+          , "        g"
+          ]
+        output =
+          [ TLet
+          , TLbrace
+          , TId "f"
+          , TEq
+          , TLbrace
+          , TLet
+          , TLbrace
+          , TId "g"
+          , TId "x"
+          , TEq
+          , TLbrace
+          , TInteger 1
+          , TRbrace
+          , TRbrace
+          , TSemicolon
+          , TId "g"
+          , TRbrace
+          , TRbrace
+          ]
+    scanTokenTypes input `shouldBe` Right output
