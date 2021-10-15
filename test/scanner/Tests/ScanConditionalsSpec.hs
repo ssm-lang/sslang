570edd495d7014ad5b4b1087b6b4af477a8fc8ac
Hans Montero <hjm2133@columbia.edu>
2021-10-15 01:42:24 -0400

    Implement some conditional scanner tests

--- test/scanner/Tests/ScanConditionalsSpec.hs
+++ test/scanner/Tests/ScanConditionalsSpec.hs
@@ -14,13 +14,53 @@ import           Front.Token                    ( TokenType(..) )
 spec :: Spec
 spec = do
   it "scans inline conditionals" $ do
-    pendingWith "help needed"
+    let input = "if a { b }"
+        output =
+          [ TIf
+          , TId "a" -- TODO(hans): What happened to TThen?
+          , TLbrace
+          , TId "b"
+          , TRbrace
+          ]
+    scanTokenTypes input `shouldBe` Right output
 
   it "scans conditionals blocks" $ do
-    pendingWith "help needed"
+    let input = unlines
+          [ "if a"
+          , "  b"
+          ]
+        output =
+          [ TIf
+          , TId "a"
+          , TThen
+          , TLbrace
+          , TId "b"
+          , TRbrace
+          ]
+    scanTokenTypes input `shouldBe` Right output
 
   it "scans loops blocks" $ do
-    pendingWith "help needed"
+    let input = unlines
+          [ "loop"
+          , "  some_action"
+          , "  some_other_action"
+          ]
+        output =
+          [ TLoop
+          , TLbrace
+          , TId "some_action"
+          , TSemicolon
+          , TId "some_other_action"
+          , TRbrace
+          ]
+    scanTokenTypes input `shouldBe` Right output
 
   it "scans inline loops" $ do
-    pendingWith "help needed"
+    let input = "loop some_action"
+        output  =
+          [ TLoop
+          , TLbrace
+          , TId "some_action"
+          , TRbrace
+          ]
+    scanTokenTypes input `shouldBe` Right output
