eb3720b3a476159dcbc97d562bdde76e259c4e0e
Hans Montero <hjm2133@columbia.edu>
2021-11-08 18:08:23 -0500

    Improve free variable collection

--- src/IR/LambdaLift.hs
+++ src/IR/LambdaLift.hs
@@ -18,7 +18,7 @@ liftProgramLambdas
   :: I.Program Poly.Type -> Compiler.Pass (I.Program Poly.Type)
 liftProgramLambdas p = do
   let defs = I.programDefs p
-      globalScope = map (\(v, _) -> show v) defs
+      globalScope = map (\(v, _) -> v) defs
       funs = filter isFun defs
       freeVars = map (getFrees (S.fromList globalScope) (S.fromList globalScope)) (map snd funs)
   traceM (show $ zip (map (\(v, _) -> v) funs) freeVars)
@@ -26,12 +26,17 @@ liftProgramLambdas p = do
     where
       isFun (_, I.Lambda _ _ _) = True
       isFun _ = False
-      getFrees scp gs (I.Var v _) = if S.member (show v) scp then [] else [show v]
+      getFrees scp gs (I.Var v _) = if S.member (v) scp then [] else [show v]
       getFrees scp gs (I.App e1 e2 _) = getFrees scp gs e1 ++ getFrees scp gs e2
-      getFrees scp gs (I.Let binds e _) = let newScp = foldl (\s (Just v, _) -> S.insert (show v) s) scp binds in
-                                        (concatMap (\(_, be) -> getFrees newScp gs be) binds) ++ getFrees newScp gs e
-      getFrees scp gs (I.Lambda (Just v) e _) = let newScp = S.insert (show v) gs in
-                                                trace (intercalate "\n" (getFrees newScp gs e) ++ "\n--") []
+      getFrees scp gs (I.Let binds e _) = let newScp = foldl (\s (Just v, _) -> S.insert (v) s) scp binds in
+                                        (concatMap (getLetFrees newScp gs) binds) ++ getFrees newScp gs e
+      getFrees scp gs lam@(I.Lambda _ _ _) = let (vs, body) = I.collectLambda lam 
+                                                 newScp = foldl (\s (Just v) -> S.insert v s) gs vs in
+                                             trace (intercalate "\n" (getFrees newScp gs body) ++ "\n--") []
       getFrees scp gs (I.Match _ _ _ _) = undefined
       getFrees scp gs (I.Prim _ es _) = concatMap (getFrees scp gs) es
       getFrees _ _ _ = []
+      getLetFrees scp gs (Just v, lam@(I.Lambda _ _ _ )) = let (vs, body) = I.collectLambda lam
+                                                               newScp = foldl (\s (Just v) -> S.insert v s) gs vs in
+                                                           trace (show v ++ ":\n" ++ (intercalate "\n" (getFrees newScp gs body) ++ "\n--")) [] 
+      getLetFrees scp gs (Just _, e) = getFrees scp gs e
