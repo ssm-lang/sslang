6c7f311f3567919b1dc92b9d1750846ab1edc337
Xijiao Li <xl2950@columbia.edu>
2021-11-09 18:10:55 -0500

    add comments

--- src/IR/TypeInference.hs
+++ src/IR/TypeInference.hs
@@ -49,18 +49,23 @@ newtype InferFn a = InferFn (StateT TypeCtx Compiler.Pass a)
   deriving (MonadError Compiler.Error)  via (StateT TypeCtx Compiler.Pass)
   deriving (MonadState TypeCtx)         via (StateT TypeCtx Compiler.Pass)
 
+-- | Run a InferFn computation.
 runInferFn :: InferFn a -> Compiler.Pass a
 runInferFn (InferFn m) = evalStateT m TypeCtx { varMap = M.empty }
 
+-- | Insert a variable ID and its type into current typing context.
 insertVar :: I.VarId -> Classes.Type -> InferFn ()
 insertVar v t = modify $ \st -> st { varMap = M.insert v t $ varMap st }
 
+-- | Replace the current typing context with a new one.
 replaceCtx :: TypeCtx -> InferFn ()
 replaceCtx ctx = modify $ const ctx
 
+-- | Look up the type of a variable in the current typing context given its variable ID.
 lookupVar :: I.VarId -> InferFn (Maybe Classes.Type)
 lookupVar v = M.lookup v <$> gets varMap
 
+-- | Infer all the program defs of the given porgram.
 inferProgram :: I.Program Ann.Type -> Compiler.Pass (I.Program Classes.Type)
 inferProgram p = runInferFn $ do
   defs' <- inferTop $ I.programDefs p
@@ -69,6 +74,12 @@ inferProgram p = runInferFn $ do
                      , I.typeDefs     = [] -- TODO: something with I.typeDefs p
                      }
 
+{- | Top level inference.
+
+This will infer the (VarId, Expr) pair sequentially and add the infered types
+into the type context so that these info can be used when infering the following
+expressions.
+-}
 inferTop :: [(I.VarId, I.Expr Ann.Type)] -> InferFn [(I.VarId, I.Expr Classes.Type)]
 inferTop [] = return []
 inferTop ((v, e):xs) = do
@@ -77,6 +88,13 @@ inferTop ((v, e):xs) = do
   xs' <- inferTop xs
   return $ (v, e'):xs'
 
+{- | Infer a single expression @e@.
+
+This function will also infer any nested expressions inside @e@ and all
+expressions in the return value will have Classes.Type as its type. Error will
+be thrown if the type annotation is in inconsistent with the true true or the
+expression is known.
+-}
 inferExpr :: I.Expr Ann.Type -> InferFn (I.Expr Classes.Type)
 inferExpr (I.Var v t) = do
   record <- lookupVar v
@@ -119,6 +137,7 @@ inferExpr e@(I.App a b _) = do
     Nothing -> throwError $ Compiler.TypeError $ "Unable to type App expression: " ++ show e
 inferExpr e = throwError $ Compiler.TypeError $ "Unable to type unknown expression: " ++ show e
 
+-- | Infer the type of a Primitive expression.
 inferPrim :: I.Expr Ann.Type -> InferFn (I.Expr Classes.Type)
 inferPrim (I.Prim I.New [e] _) = do
   e' <- inferExpr e
@@ -170,6 +189,7 @@ inferPrim (I.Prim I.Par es _) = do
     return $ I.Prim I.Par es' $ tuple ts
 inferPrim e = throwError $ Compiler.TypeError $ "Unable to type Prim expression: " ++ show e
 
+-- | Helper function to support local modificaton of type context.
 withNewScope :: InferFn a -> InferFn a
 withNewScope inf = do
   ctx <- get
@@ -177,6 +197,7 @@ withNewScope inf = do
   modify $ const ctx
   return x
 
+-- | Helper function to do type inference with the knowledge of the correct type for a binding.
 withVty :: I.Expr Ann.Type -> Binder -> Ann.Type -> InferFn ()
 withVty _ Nothing _ = return ()
 withVty e (Just v) t =
@@ -186,10 +207,17 @@ withVty e (Just v) t =
       insertVar v vty'
     Nothing -> throwError $ Compiler.TypeError $ "Lambda wasn't annotated (or it wasn't an arrow type): " ++ show e
 
+-- | Transfrom an Ann.Type to Classes.Type.
 anns2Class :: Ann.Type -> InferFn Classes.Type
 anns2Class (Ann.Type []) = throwError $ Compiler.TypeError "Cannot change empty Ann type to Classes type"
 anns2Class (Ann.Type ts) = ann2Class $ head ts
 
+{- Transfrom an Ann.TypeAnnote to Classes.Type.
+
+Since we are doing this in a monadic way, and Builtin is also a Functor, we have
+to do explicit pattern matching on the `Ann.TBuiltin bty` case to transform
+`Builtin Ann.Type` to `Builtin Classes.Type`.
+-}
 ann2Class :: Ann.TypeAnnote -> InferFn Classes.Type
 ann2Class (Ann.TBuiltin bty) = case bty of
   Unit -> return $ Classes.TBuiltin Unit
