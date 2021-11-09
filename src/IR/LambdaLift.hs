c5f7211d519753e78e96e1abe4b2bb3f8d0ee285
Hans Montero <hjm2133@columbia.edu>
2021-11-09 15:49:04 -0500

    wip: Lift lambdas using state monad

--- src/IR/LambdaLift.hs
+++ src/IR/LambdaLift.hs
@@ -1,16 +1,113 @@
+{-# LANGUAGE DerivingVia #-}
 module IR.LambdaLift where
 
 import qualified Common.Compiler               as Compiler
+import Common.Identifiers
 import qualified IR.IR                         as I
 
 import qualified IR.Types.Poly                 as Poly
 
 import Debug.Trace
+import           Control.Monad.Except           ( MonadError(..) )
+import           Control.Monad.State.Lazy       ( MonadState
+                                                , StateT(..)
+                                                , evalStateT
+                                                , gets
+                                                , get
+                                                , modify
+                                                )
 
 import qualified Data.Set as S
+import qualified Data.Map as M
 import Data.List (intercalate)
 
+data LiftCtx = LiftCtx { globalScope :: S.Set I.VarId
+                       , currentScope :: S.Set I.VarId
+                       , currentFrees :: S.Set I.VarId
+                       , lifted :: [(I.VarId, I.Expr Poly.Type)]
+                       , toAdjust :: M.Map I.VarId I.VarId
+                       , anonCount :: Int
+                       }
 
+newtype LiftFn a = LiftFn (StateT LiftCtx Compiler.Pass a)
+  deriving Functor                      via (StateT LiftCtx Compiler.Pass)
+  deriving Applicative                  via (StateT LiftCtx Compiler.Pass)
+  deriving Monad                        via (StateT LiftCtx Compiler.Pass)
+  deriving MonadFail                    via (StateT LiftCtx Compiler.Pass)
+  deriving (MonadError Compiler.Error)  via (StateT LiftCtx Compiler.Pass)
+  deriving (MonadState LiftCtx)         via (StateT LiftCtx Compiler.Pass)
+
+runLiftFn :: LiftFn a -> Compiler.Pass a
+runLiftFn (LiftFn m) = evalStateT m LiftCtx { globalScope = S.empty, currentScope = S.empty, currentFrees = S.empty, lifted = [], toAdjust = M.empty, anonCount = 0 }
+
+populateGlobalScope :: [(I.VarId, I.Expr Poly.Type)] -> LiftFn ()
+populateGlobalScope defs = do
+  let globalNames = map (\(v, _) -> v) defs
+  modify $ \st -> st { globalScope = S.fromList globalNames }
+
+inCurrentScope :: I.VarId -> LiftFn Bool
+inCurrentScope v = S.member v <$> gets currentScope
+
+getFresh :: LiftFn Int
+getFresh = do
+  curCount <- gets anonCount
+  modify $ \st -> st { anonCount = anonCount st + 1 }
+  return curCount
+
+addLifted :: String -> I.Expr Poly.Type -> LiftFn ()
+addLifted name lam = modify $ \st -> st { lifted = ((I.VarId (Identifier name)), lam) : lifted st }
+
+addFreeVar :: I.VarId -> LiftFn ()
+addFreeVar v = modify $ \st -> st { currentFrees = S.insert v $ currentFrees st }
+
+newScope :: [I.VarId] -> LiftFn ()
+newScope vs = modify $ \st -> st { currentScope = S.union (globalScope st) (S.fromList vs), currentFrees = S.empty }
+
+makeLiftedLambda :: [I.Binder] -> I.Expr Poly.Type -> Poly.Type -> LiftFn (I.Expr Poly.Type)
+makeLiftedLambda [] body _ = return body
+makeLiftedLambda vs body t = do
+  liftedBody <- makeLiftedLambda (tail vs) body t
+  return (I.Lambda (head vs) liftedBody t)
+
+liftLambdas' :: I.Expr Poly.Type -> LiftFn (I.Expr Poly.Type)
+liftLambdas' e = do
+  newScope []
+  liftLambdas e
+
+liftLambdas :: I.Expr Poly.Type -> LiftFn (I.Expr Poly.Type)
+liftLambdas n@(I.Var v _) = do
+  isNotFree <- inCurrentScope v
+  if isNotFree then return n
+               else do addFreeVar v
+                       return n 
+liftLambdas (I.App e1 e2 t) = do 
+  liftedE1 <- liftLambdas e1
+  liftedE2 <- liftLambdas e2
+  return $ I.App liftedE1 liftedE2 t
+liftLambdas (I.Prim p exprs t) = do
+  liftedExprs <- mapM liftLambdas' exprs
+  return $ I.Prim p liftedExprs t
+liftLambdas lam@(I.Lambda _ _ t) = do
+  let (vs, body) = I.collectLambda lam
+  oldCtx <- get
+  newScope $ map (\(Just v) -> v) vs
+  liftedLamBody <- liftLambdas body
+  lamFrees <- gets currentFrees
+  liftedLam <- makeLiftedLambda (map (Just) (S.toList lamFrees) ++ vs) body t
+  freshNum <- getFresh
+  addLifted ("anon" ++ (show freshNum)) liftedLam
+  return (foldl (\app v -> I.App app (I.Var v t) t) (I.Var (I.VarId (Identifier ("anon" ++ (show freshNum)))) t) (S.toList lamFrees))
+
+liftProgramLambdas' :: I.Program Poly.Type -> Compiler.Pass (I.Program Poly.Type)
+liftProgramLambdas' p = runLiftFn $ do
+  let defs = I.programDefs p
+      funs = map snd $ filter isFun defs
+  populateGlobalScope defs
+  funsWithoutLambdas <- mapM liftLambdas funs
+  return p
+    where
+      isFun (_, I.Lambda _ _ _) = True
+      isFun _ = False
 
 
 
