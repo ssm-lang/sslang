e19d416de182021a6134f29bbc326720303b08b2
Max Helman <mhh2148@columbia.edu>
2021-11-11 22:31:57 -0500

    Added scanner unit test to pattern match - it works :)

---
+++ test/scanner/Tests/ScanPatternMatchSpec.hs
@@ -0,0 +1,59 @@
+module Tests.ScanPatternMatchSpec where
+
+import           Test.Hspec                     ( Spec(..)
+                                                , it
+                                                , shouldBe
+                                                , pending
+                                                )
+
+import           Front.Scanner                  ( scanTokenTypes )
+import           Front.Token                    ( TokenType(..) )
+
+
+
+spec :: Spec
+spec = do
+  it "scans pattern matches" $ do
+    let input = unlines
+          [ "main (x: Int, clk : &Int) ="
+          , "  match x"
+          , "    id => wait clk"
+          , "    _ => wait clk"
+          ]
+        output =
+            [TId "main",
+            TLparen,
+            TId "x",
+            TColon,
+            TId "Int",
+            TComma,
+            TId "clk",
+            TColon,
+            TAmpersand,
+            TId "Int",
+            TRparen,
+            TEq,
+            TLbrace,
+            TMatch,
+            TId "x",
+            TLbrace,
+            TId "id",
+            TDRarrow,
+            TLbrace,
+            TWait,
+            TLbrace,
+            TId "clk",
+            TRbrace,
+            TRbrace,
+            TBar,
+            TUnderscore,
+            TDRarrow,
+            TLbrace,
+            TWait,
+            TLbrace,
+            TId "clk",
+            TRbrace,
+            TRbrace,
+            TRbrace,
+            TRbrace]
+    scanTokenTypes input `shouldBe` Right output
