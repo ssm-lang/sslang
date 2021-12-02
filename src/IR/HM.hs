56f9e859e5478593766b1883c359041897422e0a
Xijiao Li <xl2950@columbia.edu>
2021-12-02 03:02:58 -0500

    hindley milner type inference v1.1

--- src/IR/HM.hs
+++ src/IR/HM.hs
@@ -3,6 +3,7 @@
 module IR.HM where
 
 import qualified Data.Map                      as M
+import qualified Data.Set                      as S
 
 import qualified Common.Compiler               as Compiler
 
@@ -27,84 +28,141 @@ import           Control.Monad.State.Lazy       ( MonadState
                                                 , modify
                                                 )
 
--- | A type substitution: a mapping of type variables to concrete types
-type Uft = [(Classes.Type, Classes.Type)]
+-- | Inference State.
+data InferState = InferState
+  { varMap :: M.Map I.VarId Classes.Scheme
+  , equations :: [(Classes.Type, Classes.Type)]
+  , unionFindTree :: M.Map Classes.Type Classes.Type
+  , count :: Int
+  }
 
-data InferState = InferState  { equations :: [(Classes.Type, Classes.Type)]
-                              , subs :: M.Map Classes.Type Classes.Type
-                              , count :: Int
-                              , uft :: M.Map Classes.Type Classes.Type
-                              , varMap :: M.Map I.VarId Classes.Type
-                              }
-
--- Inference Monad
+-- | Inference Monad.
 newtype InferFn a = InferFn (StateT InferState Compiler.Pass a)
   deriving Functor                      via (StateT InferState Compiler.Pass)
   deriving Applicative                  via (StateT InferState Compiler.Pass)
   deriving Monad                        via (StateT InferState Compiler.Pass)
   deriving MonadFail                    via (StateT InferState Compiler.Pass)
   deriving (MonadError Compiler.Error)  via (StateT InferState Compiler.Pass)
-  deriving (MonadState InferState)         via (StateT InferState Compiler.Pass)
+  deriving (MonadState InferState)      via (StateT InferState Compiler.Pass)
 
--- | Run a InferFn computation.
+-- | `runInferFn` runs a InferFn computation.
 runInferFn :: InferFn a -> Compiler.Pass a
-runInferFn (InferFn m) = evalStateT m InferState { subs = M.empty
+runInferFn (InferFn m) = evalStateT m InferState { unionFindTree = M.empty
                                                  , varMap = M.empty
                                                  , count = 0
-                                                 , uft = M.empty
-                                                 , equations = []}
+                                                 , equations = [] }
 
+-- | `inferProgram` @p@ infers the type of all the programDefs of the given porgram @p@.
 inferProgram :: I.Program Ann.Type -> Compiler.Pass (I.Program Classes.Type)
 inferProgram p = runInferFn $ do
   defs' <- inferProgramDefs $ I.programDefs p
   return $ I.Program { I.programDefs  = defs'
                      , I.programEntry = I.programEntry p
-                     , I.typeDefs     = [] } -- TODO: something with I.typeDefs p  }
+                     , I.typeDefs     = [] }
 
+-- | `inferProgramDefs` @ds@ infers the type of programDefs @ds@ recursively and binds each varibale to its type.
 inferProgramDefs :: [(I.VarId, I.Expr Ann.Type)] -> InferFn [(I.VarId, I.Expr Classes.Type)]
 inferProgramDefs [] = return []
 inferProgramDefs ((v, e):xs) = do
-  e' <- initTypes e
-  eqs <- gets equations
-  unifyAll eqs
-  modify $ \st -> st { equations = []}
-  -- eqs' <- gets equations
-  -- sb' <- gets subs
-  -- throwError $ Compiler.TypeError $ "dbg: " ++ show eqs' ++ show sb'
+  e' <- initTypeVars e
+  unifyAll
   e'' <- getType e'
-  insertVar v (extract e'')
+  s <- generalize $ extract e''
+  insertVar v s
   xs' <- inferProgramDefs xs
   return $ (v, e''):xs'
+
+-- | Class of things that may contain free type variables.
+class HasFreeTVars a where
+  freeTVars :: a -> S.Set TVarIdx
+
+instance HasFreeTVars TVarIdx where
+  freeTVars = S.singleton
+
+instance HasFreeTVars Classes.Scheme where
+  {- | Determines the set of free type variables inside a scheme @Forall ns t@.
+
+  The free variables are those in the underlying type @t@ minus the
+  variables quantified over by the scheme in @ns@.
+  -}
+  freeTVars (Classes.Forall ns t) = S.difference (freeTVars t) (S.fromList ns)
+
+instance HasFreeTVars Classes.Type where
+  freeTVars (Classes.TBuiltin b) = freeTVars b
+  freeTVars (Classes.TCon _ ts)  = freeTVars ts
+  freeTVars (Classes.TVar n)     = S.singleton n
+
+instance HasFreeTVars t => HasFreeTVars (Builtin t) where
+  freeTVars Unit         = S.empty
+  freeTVars Void         = S.empty
+  freeTVars (Ref t     ) = freeTVars t
+  freeTVars (Arrow l r ) = S.union (freeTVars l) (freeTVars r)
+  freeTVars (Tuple tys ) = freeTVars tys
+  freeTVars (Integral _) = S.empty
+
+instance HasFreeTVars a => HasFreeTVars [a] where
+  -- | Lifts 'freeTVars' to lists.
+  freeTVars = foldr (S.union . freeTVars) S.empty
+
+{- | Instantiates the type from the scheme @Forall vs t@.
+
+Create fresh type variable for each quantified type variable in @vs@ and
+substitute them in @t@ by applying @subst@ to @t@.
+-}
+instantiate :: Classes.Scheme -> InferFn Classes.Type
+instantiate (Classes.Forall ns t) = do
+  ns' <- mapM (const fresh) ns
+  withNewTypeScope do
+    modify $ \st -> st { unionFindTree = M.fromList (zip (map Classes.TVar ns) ns') }
+    solveType t
+
+{- | Generalizes the type @t@ into a type scheme given the typing context @ctx@.
+
+Collect all the free type variables in the type @t@ and all the free type
+variables in the typing context @ctx@,
+away any type variables that appear free in the type environment, and whatever
+is leftover can be generalized into a type scheme.
+-}
+generalize :: Classes.Type -> InferFn Classes.Scheme
+generalize t = do
+  vm <- gets varMap
+  uft <- gets unionFindTree
+  let vs = S.toList $ S.difference (freeTVars t) $ S.union (freeTVars (M.elems uft)) (freeTVars (M.elems vm))
+  return $ Classes.Forall vs t
 
 vnums :: [Int]
 vnums = [0..]
 
+-- | `fresh` generates a new `TVar` using the next tick number and increment the counter.
 fresh :: InferFn Classes.Type
 fresh = do
   n <- gets count
   let t = Classes.TVar $ TVarIdx (vnums !! n)
-  modify $ \st -> st { count = n+1 , uft = M.insert t t $ uft st}
+  modify $ \st -> st { count = n+1 }
   return t
 
--- | Look up the type of a variable in the current typing context given its variable ID.
-lookupVar :: I.VarId -> InferFn (Maybe Classes.Type)
+-- | `lookupVar` @v@ looks up the type scheme of a variable in the current `varMap` given its variable ID @v@.
+lookupVar :: I.VarId -> InferFn (Maybe Classes.Scheme)
 lookupVar v = M.lookup v <$> gets varMap
 
--- | Insert a variable ID and its type into current typing context.
-insertVar :: I.VarId -> Classes.Type -> InferFn ()
+-- | `insertVar` @v t@ Insert a variable ID and its type scheme into current `varMap`.
+insertVar :: I.VarId -> Classes.Scheme -> InferFn ()
 insertVar v t = modify $ \st -> st { varMap = M.insert v t $ varMap st }
 
-insertBinder :: Binder -> Classes.Type -> InferFn ()
+-- | `insertBinder` is a wrapper of `insertVar` for `Binder` type.
+insertBinder :: Binder -> Classes.Scheme -> InferFn ()
 insertBinder Nothing _ = return ()
 insertBinder (Just vid) t = insertVar vid t
 
-insertEquations :: (Classes.Type, Classes.Type) -> InferFn ()
-insertEquations e = modify $ \st -> st { equations = e : equations st }
+-- | `insertEquation` @e@ inserts @e@ into the current `equations` list.
+insertEquation :: (Classes.Type, Classes.Type) -> InferFn ()
+insertEquation e = modify $ \st -> st { equations = e : equations st }
 
-insertSubst :: Classes.Type -> Classes.Type -> InferFn ()
-insertSubst t1 t2 = modify $ \st -> st { subs = M.insert t1 t2 $ subs st}
+-- | `insertUnion` @t1 t2@ inserts @t1 : t2@ into the current `unionFindTree`.
+insertUnion :: Classes.Type -> Classes.Type -> InferFn ()
+insertUnion t1 t2 = modify $ \st -> st { unionFindTree = M.insert t1 t2 $ unionFindTree st}
 
--- | Helper function to support local modificaton of type context.
+-- | Helper function to support local modificaton of `varMap`.
 withNewScope :: InferFn a -> InferFn a
 withNewScope inf = do
   vm <- gets varMap
@@ -112,124 +170,146 @@ withNewScope inf = do
   modify $ \st -> st { varMap = vm }
   return x
 
-initTypes :: I.Expr Ann.Type -> InferFn (I.Expr Classes.Type)
-initTypes e@(I.Var v _) = do
+-- | Helper function to support local modificaton of `unionFindTree`.
+withNewTypeScope :: InferFn a -> InferFn a
+withNewTypeScope inf = do
+  s <- gets unionFindTree
+  x <- inf
+  modify $ \st -> st { unionFindTree = s }
+  return x
+
+{- | Stage 1: Assign symbolic typenames
+
+`initTypeVars` @e@ walks into the expression @e@ revursively, assign a fresh
+`TVar` to each unknown type, and build type equations that will be solved later.
+-}
+initTypeVars :: I.Expr Ann.Type -> InferFn (I.Expr Classes.Type)
+initTypeVars e@(I.Var v _) = do
   record <- lookupVar v
   case record of
     Nothing -> throwError $ Compiler.TypeError $ "Unbound variable: " ++ show e
-    Just t' -> return $ I.Var v t' -- TODO: check anns
-initTypes (I.Lambda v b _) = do
+    Just s -> do
+      t' <- instantiate s
+      return $ I.Var v t'
+initTypeVars (I.Lambda v b _) = do
   tin <- fresh
   b' <- withNewScope do
-    insertBinder v tin
-    initTypes b
+    insertBinder v (Classes.Forall [] tin)
+    initTypeVars b
   return $ I.Lambda v b' (Classes.TBuiltin $ Arrow tin (extract b'))
-initTypes (I.Let vs b _) = do
-  (vs', b') <- withNewScope $ localInitTypes vs b
+initTypeVars (I.Let vs b _) = do
+  (vs', b') <- withNewScope $ localinitTypeVars vs b
   return $ I.Let vs' b' (extract b')
   where
-    localInitTypes bs body = do
+    localinitTypeVars bs body = do
       bs' <- mapM fn bs
-      body' <- initTypes body
+      body' <- initTypeVars body
       return (bs', body')
       where
         fn (binder, e) = do
-          e' <- initTypes e
-          insertBinder binder (extract e')
+          e' <- initTypeVars e
+          unifyAll
+          e'' <- getType e'
+          s <- generalize $ extract e''
+          insertBinder binder s
           return (binder, e')
-initTypes e@I.Prim {} = initPTypes e
-initTypes (I.Lit I.LitEvent _) = return $ I.Lit I.LitEvent unit
-initTypes (I.Lit i@(I.LitIntegral _) _) = return $ I.Lit i (int 32)
-initTypes (I.App a b _) = do
+initTypeVars (I.Lit I.LitEvent _) = return $ I.Lit I.LitEvent unit
+initTypeVars (I.Lit i@(I.LitIntegral _) _) = return $ I.Lit i (int 32)
+initTypeVars (I.App a b _) = do
   t <- fresh
-  a' <- initTypes a
-  b' <- initTypes b
-  insertEquations (Classes.TBuiltin (Arrow (extract b') t), extract a')
+  a' <- initTypeVars a
+  b' <- initTypeVars b
+  insertEquation (Classes.TBuiltin (Arrow (extract b') t), extract a')
   return $ I.App a' b' t
-initTypes e = throwError $ Compiler.TypeError $ "Unable to type unknown expression: " ++ show e
-
 -- | Infer the type of a Primitive expression.
-initPTypes :: I.Expr Ann.Type -> InferFn (I.Expr Classes.Type)
-initPTypes (I.Prim I.New [e] _) = do
+initTypeVars (I.Prim I.New [e] _) = do
   t <- fresh
-  e' <- initTypes e
-  insertEquations (t, Classes.TBuiltin (Ref (extract e')))
+  e' <- initTypeVars e
+  insertEquation (t, Classes.TBuiltin (Ref (extract e')))
   return $ I.Prim I.New [e'] t
-initPTypes (I.Prim I.Deref [e] _) = do
+initTypeVars (I.Prim I.Deref [e] _) = do
   t <- fresh
-  e' <- initTypes e
-  insertEquations (Classes.TBuiltin (Ref t), extract e')
+  e' <- initTypeVars e
+  insertEquation (Classes.TBuiltin (Ref t), extract e')
   return $ I.Prim I.Deref [e'] t
-initPTypes (I.Prim I.Dup [e] _) = do
+initTypeVars (I.Prim I.Dup [e] _) = do
   t <- fresh
-  e' <- initTypes e
-  insertEquations (t, extract e')
+  e' <- initTypeVars e
+  insertEquation (t, extract e')
   return $ I.Prim I.Dup [e'] t
-initPTypes (I.Prim I.Assign [lhs, rhs] _) = do
-  lhs' <- initTypes lhs
-  rhs' <- initTypes rhs
-  insertEquations (extract lhs', Classes.TBuiltin (Ref (extract rhs')))
+initTypeVars (I.Prim I.Assign [lhs, rhs] _) = do
+  lhs' <- initTypeVars lhs
+  rhs' <- initTypeVars rhs
+  insertEquation (extract lhs', Classes.TBuiltin (Ref (extract rhs')))
   return $ I.Prim I.Assign [lhs', rhs'] unit
-initPTypes (I.Prim (I.PrimOp I.PrimSub) [e1, e2]  _) = do
-  e1' <- initTypes e1
-  e2' <- initTypes e2
-  insertEquations (extract e1', int 32)
-  insertEquations (extract e2', int 32)
+initTypeVars (I.Prim (I.PrimOp I.PrimSub) [e1, e2]  _) = do
+  e1' <- initTypeVars e1
+  e2' <- initTypeVars e2
+  insertEquation (extract e1', int 32)
+  insertEquation (extract e2', int 32)
   return $ I.Prim (I.PrimOp I.PrimSub) [e1', e2'] $ int 32
-initPTypes (I.Prim (I.PrimOp I.PrimAdd) [e1, e2]  _) = do
-  e1' <- initTypes e1
-  e2' <- initTypes e2
-  insertEquations (extract e1', int 32)
-  insertEquations (extract e2', int 32)
+initTypeVars (I.Prim (I.PrimOp I.PrimAdd) [e1, e2]  _) = do
+  e1' <- initTypeVars e1
+  e2' <- initTypeVars e2
+  insertEquation (extract e1', int 32)
+  insertEquation (extract e2', int 32)
   return $ I.Prim (I.PrimOp I.PrimAdd) [e1', e2'] $ int 32
-initPTypes (I.Prim I.After [del, lhs, rhs] _) = do
-  del' <- initTypes del
-  rhs' <- initTypes rhs
-  lhs' <- initTypes lhs
-  insertEquations (extract del', int 32)
-  insertEquations (extract lhs', Classes.TBuiltin (Ref (extract rhs')))
+initTypeVars (I.Prim I.After [del, lhs, rhs] _) = do
+  del' <- initTypeVars del
+  rhs' <- initTypeVars rhs
+  lhs' <- initTypeVars lhs
+  insertEquation (extract del', int 32)
+  insertEquation (extract lhs', Classes.TBuiltin (Ref (extract rhs')))
   return $ I.Prim I.After [del', lhs', rhs'] unit
-initPTypes (I.Prim I.Break [] _) = return $ I.Prim I.Break [] void
-initPTypes (I.Prim I.Return [] _) = return $ I.Prim I.Return [] void
-initPTypes (I.Prim I.Loop es _) = do
-    es' <- mapM initTypes es
+initTypeVars (I.Prim I.Break [] _) = return $ I.Prim I.Break [] void
+initTypeVars (I.Prim I.Return [] _) = return $ I.Prim I.Return [] void
+initTypeVars (I.Prim I.Loop es _) = do
+    es' <- mapM initTypeVars es
     return $ I.Prim I.Loop es' unit
-initPTypes (I.Prim I.Wait es _) = do
-    es' <- mapM initTypes es
+initTypeVars (I.Prim I.Wait es _) = do
+    es' <- mapM initTypeVars es
     return $ I.Prim I.Wait es' unit
-initPTypes (I.Prim I.Par es _) = do
-    es' <- mapM initTypes es
-    return $ I.Prim I.Par es' unit
-initPTypes e = throwError $ Compiler.TypeError $ "Unsupported Prim expression: " ++ show e
+initTypeVars (I.Prim I.Par es _) = do
+    es' <- mapM initTypeVars es
+    return $ I.Prim I.Par es' $ Classes.TBuiltin (Tuple (map extract es'))
+initTypeVars e@I.Prim {} = throwError $ Compiler.TypeError $ "Unsupported Prim expression: " ++ show e
+initTypeVars e = throwError $ Compiler.TypeError $ "Unable to type unknown expression: " ++ show e
 
-unifyAll :: [(Classes.Type, Classes.Type)] -> InferFn ()
-unifyAll [] = return ()
-unifyAll ((l,r):eqs) = do
-  unify l r
-  unifyAll eqs
+{- | Stage 2: Solve type equations using unification.
 
+`unifyAll` @eqs@ solve the list of equations @eqs@ one by one and puts solutions
+as new enties in the `unionFindTree`.
+-}
+unifyAll :: InferFn ()
+unifyAll = do
+  eq <- gets equations
+  case eq of
+    [] -> return ()
+    ((l,r):eqs) -> do
+      unify l r
+      modify $ \st -> st { equations = eqs }
+      unifyAll
+
+-- |`unify` @t1 t2@ solves the type equation t1 ~ t2 and put the solution into `unionFindTree`.
 unify :: Classes.Type -> Classes.Type -> InferFn ()
 unify t1 t2
   | t1 == t2 = return ()
 unify tv1@(Classes.TVar _) tv2@(Classes.TVar _) = do
   r1 <- findRoot tv1
   r2 <- findRoot tv2
-  if r1 == tv1 && r2 == tv2 then insertSubst r1 r2 else unify r1 r2
+  if r1 == tv1 && r2 == tv2 then insertUnion r1 r2 else unify r1 r2
 unify t tv@(Classes.TVar _) = unify tv t
 unify tv@(Classes.TVar _) t = do
   r1 <- findRoot tv
-  if r1 == tv then insertSubst r1 t else unify r1 t
+  if r1 == tv then insertUnion r1 t else unify r1 t
 unify (Classes.TBuiltin (Ref t1)) (Classes.TBuiltin (Ref t2)) = unify t1 t2
 unify (Classes.TBuiltin (Arrow t1 t2)) (Classes.TBuiltin (Arrow t3 t4)) = unify t1 t3 >> unify t2 t4
 unify _ _ = throwError $ Compiler.TypeError "Single unification error"
 
-findRoot :: Classes.Type -> InferFn Classes.Type
-findRoot t = do
-  sb <- gets subs
-  case M.lookup t sb of
-    Nothing -> return t
-    Just t' -> findRoot t'
+{- | Stage 3: Find the type of the expression based on `unionFindTree`.
 
+`getType` @e@ walks into the expression @e@ and solves its type @t@ recursively.
+-}
 getType :: I.Expr Classes.Type -> InferFn (I.Expr Classes.Type)
 getType (I.Var v t) = do
   t' <- solveType t
@@ -292,9 +372,11 @@ getType (I.Prim I.Wait es _) = do
     return $ I.Prim I.Wait es' unit
 getType (I.Prim I.Par es _) = do
     es' <- mapM getType es
-    return $ I.Prim I.Par es' unit
-getType e = throwError $ Compiler.TypeError $ "Unable to get type of unknown expression: " ++ show e
+    return $ I.Prim I.Par es' $ Classes.TBuiltin (Tuple (map extract es'))
+getType e = throwError $ Compiler.TypeError $ "Unable to get the type of unknown expression: " ++ show e
 
+-- | `solveType` @t@ solves the type @t@ by replacing any embeded `TVar` @tvar@
+--   with its real type, which is the root of @tvar@ in the `unionFindTree`.
 solveType :: Classes.Type -> InferFn Classes.Type
 solveType t@(Classes.TBuiltin (Integral 32)) = return t
 solveType t@(Classes.TBuiltin Unit) = return t
@@ -311,3 +393,12 @@ solveType (Classes.TBuiltin (Arrow t1 t2)) = do
   t2' <- solveType t2
   return $ Classes.TBuiltin $ Arrow t1' t2'
 solveType t = throwError $ Compiler.TypeError $ "Solve Type error" ++ show t
+
+-- | `findRoot` @t@ finds the root of @t@ inside `unionFindTree`.
+findRoot :: Classes.Type -> InferFn Classes.Type
+findRoot t = do
+  sb <- gets unionFindTree
+  -- We find the root of @t@ if @t@ exists in the tree as a key and its value is different from itself
+  case M.lookup t sb of
+    Just t' | t' /= t -> findRoot t'
+    _ -> return t
