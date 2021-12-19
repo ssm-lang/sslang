64664ed592ea4bfa3b52de1efeb4c236c27cab79
Leo Qiao <qiaofeitong@hotmail.com>
2021-12-19 14:33:59 -0500

    implement instance instantiation

--- src/IR/ClassInstantiation.hs
+++ src/IR/ClassInstantiation.hs
@@ -2,15 +2,19 @@ module IR.ClassInstantiation where
 
 import qualified Common.Compiler               as Compiler
 import           Common.Identifiers                       ( FieldId
+                                                          , Identifier(..)
                                                           , TConId(..)
                                                           , fromString
                                                           )
+import           Control.Comonad                          ( extract )
 import           Data.Bifunctor                           ( second )
+import           Data.Foldable                            ( find )
 import           IR.IR
 import qualified IR.Types.Classes              as Classes
 import qualified IR.Types.Poly                 as Poly
 import           IR.Types.TypeSystem                      ( TypeDef(..)
                                                           , TypeVariant(..)
+                                                          , arrow
                                                           )
 
 instProgram :: Program Classes.Type -> Compiler.Pass (Program Poly.Type)
@@ -19,13 +23,9 @@ instProgram Program { programEntry = pEntry, programDefs = pdefs, programTypes =
         let tdefs' = fmap (second $ fmap class2Poly) tdefs
             pdefs' = fmap (second $ fmap class2Poly) pdefs
         cdefs' <- instClasses cdefs
-        -- idefs' <- instInsts idefs pdefs
-        _      <- if null idefs
-            then return idefs
-            else Compiler.throw
-                $ Compiler.TypeError "can't instantiate class instances yet"
+        idefs' <- instInsts cdefs idefs
         return $ Program { programEntry   = pEntry
-                         , programDefs    = pdefs'
+                         , programDefs    = idefs' ++ pdefs'
                          , programTypes   = cdefs' ++ tdefs'
                          , programClasses = []
                          , programInsts   = []
@@ -56,15 +56,63 @@ instClassMethod (mName, mType) =
     return (fromString $ show mName, class2Poly mType)
 
 instInsts
-    :: [InstDef Classes.Type]
-    -> [(VarId, Expr Classes.Type)]
+    :: [ClassDef Classes.Type]
+    -> [InstDef Classes.Type]
     -> Compiler.Pass [(VarId, Expr Poly.Type)]
--- instInsts _ = fmap (second $ fmap class2Poly)
-instInsts _ _ = Compiler.throw
-    $ Compiler.TypeError "instance instantiation: not implemented"
+instInsts cdefs idefs = do
+    mapM instInst' idefs
+  where
+    instInst' :: InstDef Classes.Type -> Compiler.Pass (VarId, Expr Poly.Type)
+    instInst' idef =
+        let clsName = case instConstraint idef of
+                IsIn _ clsName' -> clsName'
+            cdef = find (\cdef' -> className cdef' == clsName) cdefs
+        in  case cdef of
+                Just cdef' -> instInst cdef' idef
+                Nothing    -> Compiler.throw $ Compiler.TypeError
+                    "can't instantiate because class does not exist"
 
+
+instInst
+    :: ClassDef Classes.Type
+    -> InstDef Classes.Type
+    -> Compiler.Pass (VarId, Expr Poly.Type)
+instInst cdef idef = do
+    let (IsIn itype cid@(ClassId (Identifier cid'))) = instConstraint idef
+        iDataName = fromString $ instNameMangle cid itype
+        iDataType = Poly.TCon (fromString . show $ cid) [class2Poly itype]
+        methods =
+            [ find (\(imName, _) -> imName == cmName) (instMethods idef)
+            | (cmName, _) <- classMethods cdef
+            ]
+        buildType :: [Poly.Type] -> Poly.Type
+        buildType []       = iDataType
+        buildType (t : ts) = t `arrow` buildType ts
+        buildData
+            :: [Poly.Type]
+            -> [Maybe (VarId, Expr Classes.Type)]
+            -> Compiler.Pass (Expr Poly.Type)
+        buildData fieldTypes [] =
+            return $ Data (fromString cid') (buildType fieldTypes)
+        buildData fieldTypes (m : ms) = do
+            case m of
+                Just (_, body) ->
+                    let mType = class2Poly $ extract body
+                    in  do
+                            dataVal <- buildData (mType : fieldTypes) ms
+                            return $ App dataVal
+                                         (fmap class2Poly body)
+                                         (buildType fieldTypes)
+                Nothing -> Compiler.throw
+                    $ Compiler.TypeError "Instance is missing a method"
+
+    dataVal <- buildData [] (reverse methods)
+    return (iDataName, dataVal)
 
 class2Poly :: Classes.Type -> Poly.Type
 class2Poly (Classes.TBuiltin bty) = Poly.TBuiltin $ fmap class2Poly bty
 class2Poly (Classes.TCon tid tys) = Poly.TCon tid $ fmap class2Poly tys
 class2Poly (Classes.TVar tid    ) = Poly.TVar tid
+
+instNameMangle :: ClassId -> Classes.Type -> String
+instNameMangle cid instType = show cid ++ "_" ++ show instType
--- test/typeclass/Tests/ClassInstantiationSpec.hs
+++ test/typeclass/Tests/ClassInstantiationSpec.hs
@@ -8,14 +8,24 @@ import           Common.Compiler                          ( Pass
                                                           , runPass
                                                           )
 import           Common.Identifiers                       ( fromString )
-import           IR.ClassInstantiation                    ( instProgram )
-import           IR.IR                                    ( ClassDef(..) )
+import           IR.ClassInstantiation                    ( instNameMangle
+                                                          , instProgram
+                                                          )
+import           IR.IR                                    ( Alt(..)
+                                                          , ClassDef(..)
+                                                          , Expr(..)
+                                                          , Literal(..)
+                                                          )
 import           IR.Types.TypeSystem                      ( Builtin(..)
+                                                          , InstConstraint(..)
                                                           , TypeDef(..)
                                                           , TypeVariant
-                                                            ( VariantNamed
-                                                            )
+                                                              ( VariantNamed
+                                                              )
+                                                          , arrow
+                                                          , int
                                                           , projectBuiltin
+                                                          , void
                                                           )
 
 import           Test.Hspec                               ( Expectation
@@ -35,48 +45,78 @@ emptyProgram = I.Program { I.programEntry   = fromString "main"
                          }
 
 isSameProgramAs
-  :: Pass (I.Program P.Type) -> Pass (I.Program P.Type) -> Expectation
+    :: Pass (I.Program P.Type) -> Pass (I.Program P.Type) -> Expectation
 isSameProgramAs p1 p2 = case (runPass p1, runPass p2) of
-  (Right p1', Right p2') -> p1' `shouldBe` p2'
-  _                      -> expectationFailure "ClassInstantiation pass failed"
+    (Right p1', Right p2') -> p1' `shouldBe` p2'
+    _ -> expectationFailure "ClassInstantiation pass failed"
 
+_clsName = "Collapsible"
+_clsTVar = "a"
+_clsMethodName = "collapse"
+_clsMethodType = C.TBuiltin (Arrow (C.TVar (fromString _clsTVar)) (int 32))
+_cls = I.ClassDef
+    { I.className    = fromString _clsName
+    , I.classTVar    = C.TVar (fromString _clsTVar)
+    , I.classMethods = [(fromString _clsMethodName, _clsMethodType)]
+    }
+_clsADT =
+    ( fromString _clsName
+    , TypeDef
+        { arity       = 1
+        , variants    = [ ( fromString _clsName
+                          , VariantNamed
+                              [(fromString _clsMethodName, _clsMethodType)]
+                          )
+                        ]
+        , typeDefVars = [C.TVar (fromString _clsTVar)]
+        }
+    )
+
+_instType = int 32
+_instMethod = Lambda
+    (Just $ fromString "x")
+    (Match
+        (Var (fromString "x") (int 32))
+        [ (AltLit (LitIntegral 0), Lit (LitIntegral 0) (int 32))
+        , (AltDefault Nothing    , Lit (LitIntegral 1) (int 32))
+        ]
+        (int 1)
+    )
+    _clsMethodType
+
+_inst = I.InstDef { I.instConstraint = _instType `IsIn` fromString _clsName
+                  , I.instMethods = [(fromString _clsMethodName, _instMethod)]
+                  }
+
+_instData =
+    ( fromString $ instNameMangle (fromString _clsName) _instType
+    , App
+        (Data
+            (fromString _clsName)
+            (_clsMethodType `arrow` C.TCon (fromString _clsName) [_instType])
+        )
+        _instMethod
+        (C.TCon (fromString _clsName) [_instType])
+    )
 
 spec :: Spec
 spec = do
-  it "instantiates a typeclass with single parameter and single method" $ do
-    let
-      clsName       = "ExampleClass"
-      clsTVar       = "a"
-      clsMethodName = "exampleMethod"
-      clsMethodType =
-        C.TBuiltin (Arrow (C.TVar (fromString clsTVar)) (projectBuiltin Void))
-      cls = I.ClassDef
-        { I.className    = fromString clsName
-        , I.classTVar    = C.TVar (fromString clsTVar)
-        , I.classMethods =
-          [ ( fromString clsMethodName
-            , C.TBuiltin
--- instClassMethod :: (VarId, Classes.Type) -> TypeVariant Poly.Type
-              (Arrow (C.TVar (fromString clsTVar)) (projectBuiltin Void))
-            )
-          ]
-        }
-      withClass    = emptyProgram { I.programClasses = [cls] }
-      withoutClass = emptyProgram
-        { I.programTypes =
-          [ ( fromString clsName
-            , TypeDef
-              { arity       = 1
-              , variants    =
-                [ ( fromString clsName
-                  , VariantNamed [(fromString clsMethodName, clsMethodType)]
-                  )
-                ]
-              , typeDefVars = [C.TVar $ fromString clsTVar]
-              }
-            )
-          ]
-        }
-      instantiatedWithClass    = instProgram withClass
-      instantiatedWithoutClass = instProgram withoutClass
-    instantiatedWithClass `isSameProgramAs` instantiatedWithoutClass
+    it "instantiates a typeclass with single parameter and single method" $ do
+        let withClass = emptyProgram { I.programClasses = [_cls] }
+            withoutClass = emptyProgram { I.programTypes = [_clsADT] }
+            instantiatedWithClass = instProgram withClass
+            instantiatedWithoutClass = instProgram withoutClass
+        instantiatedWithClass `isSameProgramAs` instantiatedWithoutClass
+
+    it
+            "instantiates a typeclass and instance with single parameter and single method"
+        $ do
+              let withClass = emptyProgram { I.programClasses = [_cls]
+                                           , I.programInsts   = [_inst]
+                                           }
+                  withoutClass = emptyProgram { I.programTypes = [_clsADT]
+                                              , I.programDefs  = [_instData]
+                                              }
+                  instantiatedWithClass    = instProgram withClass
+                  instantiatedWithoutClass = instProgram withoutClass
+              instantiatedWithClass `isSameProgramAs` instantiatedWithoutClass
