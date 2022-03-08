a32b48077c480eeaf8284e0261e998baf0dbd9a8
EmilySillars <emsillars@gmail.com>
2022-03-08 14:08:37 -0500

    remove special casing of fully applied data constructors

--- src/IR/DConToFunc.hs
+++ src/IR/DConToFunc.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE TupleSections #-}
--- | Turns unapplied and partially applied data constructors into calls to constructor functions.
+-- | Turns non-nullary data constructors into calls to constructor functions.
 
 module IR.DConToFunc
   ( dConToFunc
@@ -62,70 +62,39 @@ createArityEnv = foldl (\acc def -> acc <> insertArities def) M.empty
     insertArity (dconid, variant) =
       M.insert dconid (variantFields variant) M.empty
 
-{- | 'dConToFunc' modifies programDefs and traverses the IR to accomplish three tasks:
+{- | 'dConToFunc' modifies programDefs and traverses the IR to accomplish two tasks:
 
-  1) Add top-level constructor functions for each 'DCon' to progamDefs
-  2) Turn unapplied data constuctors into calls to top level constructor funcs
-  3) Turn partially applied data constructors into calls to top level constructor funcs
+  1) Add top-level constructor functions for each non-nullary 'DCon' to progamDefs
+  2) Turn non-nullary data constuctors into calls to top level constructor funcs
 -}
 dConToFunc :: I.Program Poly.Type -> Compiler.Pass (I.Program Poly.Type)
 dConToFunc p@I.Program { I.programDefs = defs, I.typeDefs = tDefs } =
   runArityFn $ do
-    defs' <- local (createArityEnv tDefs <>) (mapM dataToAppW defs)
+    defs' <- local (createArityEnv tDefs <>)
+                   (mapM (\(v, e) -> (v, ) <$> dataToApp e) defs)
     return p { I.programDefs = defs' ++ concat (createFuncs <$> tDefs) }
 
--- | Wrapper for recursive dataToApp function
-dataToAppW
-  :: (I.VarId, I.Expr Poly.Type) -> ArityFn (I.VarId, I.Expr Poly.Type)
-dataToAppW (v, expr) = (v, ) <$> dataToApp expr
-  where
-  {- | Replace data constructor application with function application
+{- | Replace data constructor application with function application
 
-  Turn I.App instances of the form (I.Data _ _) into (I.Var _ _) where needed
-  -}
-  dataToApp :: I.Expr Poly.Type -> ArityFn (I.Expr Poly.Type)
-  dataToApp a@(I.Data _ _) = fst <$> dataToAppH (a, 0)
-  dataToApp (I.App left right t) =
-    I.App
-      <$> (fst <$> dataToAppH (left, 1))
-      <*> (fst <$> dataToAppH (right, 0))
-      <*> pure t
-  dataToApp (I.Let args body t) =
-    I.Let <$> mapM recurse args <*> dataToApp body <*> pure t
-    where recurse = \(b, e) -> (,) b <$> dataToApp e
-  dataToApp (I.Lambda b e t) = I.Lambda b <$> dataToApp e <*> pure t
-  dataToApp (I.Match scrut arms t) =
-    I.Match <$> dataToApp scrut <*> mapM recurse arms <*> pure t
-    where recurse = \(alt, e) -> (,) alt <$> dataToApp e
-  dataToApp (I.Prim p es t) = I.Prim p <$> mapM dataToApp es <*> pure t
-  dataToApp a               = pure a
-
-  {- | Helper function for dataToApp that keeps track of depth
-
-  This function recurses on an App node looking for a data constructor.
-
-  If a data constructor is unapplied or partially applied, its replaced with
-  the name of its constructor function, turning the App into a top-leve function call.
-
-  When a data constructor's arity <= its depth in the AST, it is fully applied. 
-  Depth is measured in terms of the number of encapsulating App nodes,
-  or how deeply the data constructor is nested inside an Application.
-  -}
-  dataToAppH :: (I.Expr Poly.Type, Int) -> ArityFn (I.Expr Poly.Type, Int)
-  dataToAppH (w@(I.Data dconid t), depth) = do
-    Just arity <- asks (M.lookup dconid)
-    if arity <= depth -- check whether data constructor is fully applied
-      then return (w, depth)
-      else pure (I.Var (I.VarId $ fromString $ "__" ++ ident dconid) t, depth)
-  dataToAppH (I.App l r t, depth) =
-    (, depth)
-      <$> (   I.App
-          <$> (fst <$> dataToAppH (l, depth + 1))
-          <*> (fst <$> dataToAppH (r, depth))
-          <*> pure t
-          )
-  dataToAppH e = pure e
-  
+  Turn I.App instances of the form (I.Data _ _) into (I.Var _ _)
+-}
+dataToApp :: I.Expr Poly.Type -> ArityFn (I.Expr Poly.Type)
+dataToApp a@(I.Data dconid t) = do
+  Just arity <- asks (M.lookup dconid)
+  case arity of
+    0 -> return a -- leave nullary data constructors alone
+    _ -> return $ I.Var (I.VarId $ fromString $ "__" ++ ident dconid) t
+dataToApp (I.App left right t) =
+  I.App <$> dataToApp left <*> dataToApp right <*> pure t
+dataToApp (I.Let args body t) =
+  I.Let <$> mapM recurse args <*> dataToApp body <*> pure t
+  where recurse = \(b, e) -> (,) b <$> dataToApp e
+dataToApp (I.Lambda b e t) = I.Lambda b <$> dataToApp e <*> pure t
+dataToApp (I.Match scrut arms t) =
+  I.Match <$> dataToApp scrut <*> mapM recurse arms <*> pure t
+  where recurse = \(alt, e) -> (,) alt <$> dataToApp e
+dataToApp (I.Prim p es t) = I.Prim p <$> mapM dataToApp es <*> pure t
+dataToApp a               = pure a
 
 {- | Worked example of ADT definition and corresponding constructor functions
 
