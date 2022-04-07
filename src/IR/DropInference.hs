a8949db982cf58d9444d2bf66af123954bd9a314
yiming-fang <yf2484@columbia.edu>
2022-04-06 22:10:41 -0400

    inserted dup to app, let, lambda, and match; add corresponding tests

--- src/IR/DropInference.hs
+++ src/IR/DropInference.hs
@@ -1,27 +1,25 @@
 {-# LANGUAGE DerivingVia #-}
 
-
 module IR.DropInference
   ( insertDropsProgram
   ) where
 
-
 import qualified Common.Compiler               as Compiler
 import           Common.Identifiers
-import qualified IR.IR                         as I
 import           Control.Monad.Except           ( MonadError(..) )
 import           Control.Monad.State.Lazy       ( MonadState
                                                 , StateT(..)
                                                 , evalStateT
+                                                , forM
                                                 , gets
                                                 , modify
-                                                , forM
                                                 )
-import qualified IR.Types.Poly                 as Poly
-import           Data.Maybe                     ( isNothing
-                                                , fromJust
+import           Data.Maybe                     ( fromJust
+                                                , isNothing
                                                 )
 import qualified Data.Set                      as S
+import qualified IR.IR                         as I
+import qualified IR.Types.Poly                 as Poly
 
 
 -- | Inserting State
@@ -65,6 +63,10 @@ getFresh str = do
 makeDrop :: I.Expr Poly.Type -> (Maybe a, I.Expr Poly.Type)
 makeDrop e = (Nothing, I.Prim I.Drop [e] $ Poly.TBuiltin Poly.Unit)
 
+-- | Make a dup primitive with unit type
+makeDup :: I.Expr Poly.Type -> (Maybe a, I.Expr Poly.Type)
+makeDup e = (Nothing, I.Prim I.Dup [e] $ I.extract e)
+
 -- | Given @a@ and @b@, construct @a ; b@ (i.e., @let _ = a ; b@). 
 seqExpr :: (Maybe I.VarId, I.Expr a) -> I.Expr a -> I.Expr a
 seqExpr a b = I.Let [a] b (I.extract b)
@@ -95,34 +97,65 @@ insertDropTop (var, expr) = do
 -- | Entry-point to inserting into expressions.
 insertDropExpr :: I.Expr Poly.Type -> InsertFn (I.Expr Poly.Type)
 
--- | Inserting drops into Let bindings
+-- | Inserting dup/drops into function application with arg of varid
+insertDropExpr (I.App fun arg@(I.Var var vt) typ) = do
+  retVar <- getFresh "_app"
+  fun'   <- insertDropExpr fun
+  arg'   <- insertDropExpr arg
+  let varDup   = makeDup $ I.Var var vt
+      varDrop  = makeDrop $ I.Var var vt
+      dupExpr  = seqExprs [varDup] (I.App fun' arg' typ)
+      dropExpr = (Just retVar, dupExpr) : [varDrop]
+      retExpr  = I.Var retVar typ
+      retExpr' = seqExprs dropExpr retExpr
+  return retExpr'
+
+-- | Inserting dup/drops into function application with arg of non-varid
+insertDropExpr (I.App fun arg typ) = do
+  fun' <- insertDropExpr fun
+  arg' <- insertDropExpr arg
+  return $ I.App fun' arg' typ
+
+-- | Inserting drops into let bindings
 insertDropExpr (I.Let bins expr typ) = do
   retVar <- getFresh "_let"
+  expr'  <- insertDropExpr expr
   bins'  <- forM bins $ \(v, d) -> do
     temp <- getFresh "_let_underscore"
     d'   <- insertDropExpr d
     return (if isNothing v then Just temp else v, d')
-  let makeDrops = \b -> makeDrop $ I.Var (fromJust $ fst b) (I.extract $ snd b)
-      exprBins  = (Just retVar, expr) : map makeDrops bins'
-      retExpr   = seqExprs exprBins $ I.Var retVar typ
-  return $ I.Let bins' retExpr typ
+  let makeFun f = \b -> f $ I.Var (fromJust $ fst b) (I.extract $ snd b)
+      dupBins  = map (makeFun makeDup) bins'
+      dropBins = map (makeFun makeDrop) bins'
+      exprBins = dupBins ++ (Just retVar, expr') : dropBins
+      retExpr  = I.Var retVar typ
+      retExpr' = seqExprs exprBins retExpr
+  return $ I.Let bins' retExpr' typ
 
 -- | Inserting drops into lambda functions
 insertDropExpr (I.Lambda var expr typ) = do
   retVar <- getFresh "_lambda"
   expr'  <- insertDropExpr expr
-  let typArg (Poly.TBuiltin(Poly.Arrow l _)) = l
+  let typArg (Poly.TBuiltin (Poly.Arrow l _)) = l
       typArg t = t
-  let varDrop  = makeDrop $ I.Var (fromJust var) (typArg typ)
-      exprBins = (Just retVar, expr') : [varDrop]
-      retExpr  = seqExprs exprBins $ I.Var retVar (typArg typ)
-  return $ I.Lambda var retExpr typ
+  let varExpr  = I.Var (fromJust var) (typArg typ)
+      varDup   = makeDup varExpr
+      varDrop  = makeDrop varExpr
+      exprBins = varDup : (Just retVar, expr') : [varDrop]
+      retExpr  = I.Var retVar (typArg typ)
+      retExpr' = seqExprs exprBins retExpr
+  return $ I.Lambda var retExpr' typ
 
 -- | Inserting drops into pattern-matching
 insertDropExpr (I.Match expr alts typ) = do
   alts' <- forM alts $ \(v, e) -> do
     insertDropAlt (v, e) expr
   return $ I.Match expr alts' typ
+
+-- | Inserting dup/drops into primitive's arguments
+insertDropExpr (I.Prim p es typ) = do
+  es' <- mapM insertDropExpr es
+  return $ I.Prim p es' typ
 
 -- | placeholder for other exprs
 insertDropExpr expr = return expr
@@ -134,25 +167,31 @@ insertDropAlt
   -> I.Expr Poly.Type
   -> InsertFn (I.Alt, I.Expr Poly.Type)
 
--- | Inserting drops into AltDefault Binder
+-- | Inserting dup/drops into AltDefault arms
 insertDropAlt (I.AltDefault var, exprReturn) exprMatch = do
   retVar      <- getFresh "_alt_default"
   exprReturn' <- insertDropExpr exprReturn
   let varDrop  = makeDrop $ I.Var (fromJust var) (I.extract exprMatch)
       exprBins = (Just retVar, exprReturn') : [varDrop]
-      retExpr  = seqExprs exprBins $ I.Var retVar (I.extract exprReturn')
-  return (I.AltDefault var, retExpr)
+      retExpr  = I.Var retVar (I.extract exprReturn')
+      retExpr' = seqExprs exprBins retExpr
+  return (I.AltDefault var, retExpr')
 
+-- | Inserting dup/drops into AltData arms
 -- | TODO: type is currently wrong and is only a placeholder
--- | to fix, will need to enumerate through all expr constructors
--- | makes more sense to do when inserting dups
-insertDropAlt (I.AltData dcon vars, exprReturn) exprMatch = do
+insertDropAlt (I.AltData dcon vars, exprReturn) _ = do
   retVar      <- getFresh "_alt_data"
   exprReturn' <- insertDropExpr exprReturn
-  let makeDrops = \v -> makeDrop $ I.Var (fromJust v) (I.extract exprMatch)
-      exprBins  = (Just retVar, exprReturn') : map makeDrops vars
-      retExpr   = seqExprs exprBins $ I.Var retVar (I.extract exprReturn')
-  return (I.AltData dcon vars, retExpr)
+  let retTyp = I.extract exprReturn'
+      makeFun f = \v -> f $ I.Var (fromJust v) retTyp
+      dupBins  = map (makeFun makeDup) vars
+      dropBins = map (makeFun makeDrop) vars
+      exprBins = dupBins ++ (Just retVar, exprReturn') : dropBins
+      retExpr  = I.Var retVar retTyp
+      retExpr' = seqExprs exprBins retExpr
+  return (I.AltData dcon vars, retExpr')
 
+-- | Inserting dup/drops into other pattern match arms
 insertDropAlt (I.AltLit l, exprReturn) _ = do
-  return (I.AltLit l, exprReturn)
+  exprReturn' <- insertDropExpr exprReturn
+  return (I.AltLit l, exprReturn')
--- test/trans/Tests/DropInferenceSpec.hs
+++ test/trans/Tests/DropInferenceSpec.hs
@@ -5,7 +5,6 @@
 -- | Sanity check test suite for equivalent and unequivalent programs.
 module Tests.DropInferenceSpec where
 import           Sslang.Test
-
 import           Data.Data                      ( Data
                                                 , Proxy(..)
                                                 )
@@ -19,6 +18,7 @@ import           IR.LambdaLift                  ( liftProgramLambdas )
 import           IR.SubstMagic                  ( substMagic )
 import           IR.Types.Annotated            as I
 import           IR.Types.Poly                 as Poly
+import Control.Concurrent (yield)
 
 
 parseDrop :: String -> Pass (I.Program I.Type)
@@ -29,7 +29,7 @@ parseDrop s = substMagic (Proxy :: Proxy I.Type) <$> do
 
 spec :: Spec
 spec = do
-  it "drop inference in named let binding" $ do
+  it "drop inference in simple let binding" $ do
     let undropped =
           parseDrop [here|
         top = 
@@ -45,10 +45,44 @@ spec = do
         top =
             let a: Int = 5
                 b: Int = 10
+            let _ = dup a
+            let _ = dup b
             let anon0_let = a + b
             let _ = drop a
             let _ = drop b
             anon0_let
+        |]
+            >>= IR.ann2Class def
+            >>= IR.class2Poly def
+    undropped `shouldPassAs` dropped
+
+  it "drop inference in nested let binding" $ do
+    let undropped =
+          parseDrop [here|
+        top = 
+            let a = 5
+                b = let c = a + 3
+                    c + 5
+            a + b
+        |]
+            >>= IR.ann2Class def
+            >>= IR.class2Poly def
+            >>= insertDropsProgram
+    let dropped =
+          parseDrop [here|
+        top =
+            let a = 5
+                b = let c = a + 3
+                    let _ = dup c
+                    let anon0_let = c + 5
+                    let _ = drop c
+                    anon0_let
+            let _ = dup a
+            let _ = dup b
+            let anon1_let = a + b
+            let _ = drop a
+            let _ = drop b
+            anon1_let
         |]
             >>= IR.ann2Class def
             >>= IR.class2Poly def
@@ -65,6 +99,7 @@ spec = do
     let dropped =
           parseDrop [here|
         add_fun a : Int -> Int = 
+          let _ = dup a
           let anon0_lambda = a + 2
           let _ = drop a
           anon0_lambda
@@ -72,3 +107,70 @@ spec = do
             >>= IR.ann2Class def
             >>= IR.class2Poly def
     undropped `shouldPassAs` dropped
+
+  it "drop inference in function application" $ do
+    let undropped =
+          parseDrop [here|
+        x : Int = 5
+        id_fun a : Int -> Int = a
+        top = id_fun x
+        |]
+            >>= IR.ann2Class def
+            >>= IR.class2Poly def
+            >>= insertDropsProgram
+    let dropped =
+          parseDrop [here|
+        x : Int = 5
+        id_fun a : Int -> Int = 
+          let _ = dup a
+          let anon0_lambda = a
+          let _ = drop a
+          anon0_lambda
+        top = let anon1_app = let _ = dup x
+                              id_fun x
+              let _ = drop x
+              anon1_app
+        |]
+            >>= IR.ann2Class def
+            >>= IR.class2Poly def
+    undropped `shouldPassAs` dropped
+
+  it "drop inference in pattern matching" $ do
+    let undropped = parseDrop [here|
+        type MyBool 
+          MyFalse Int Int
+          MyTrue Int
+
+        x = MyTrue 30
+        top = match x
+                MyTrue fst = fst
+                MyFalse fst snd = snd
+        |]
+            >>= IR.ann2Class def
+            >>= IR.class2Poly def
+            >>= insertDropsProgram
+    let dropped =
+          parseDrop [here|
+        type MyBool 
+          MyFalse Int Int
+          MyTrue Int
+
+        x = MyTrue 30
+        top = match x
+                MyTrue fst = 
+                  let _ = dup fst
+                  let anon0_alt_data = fst
+                  let _ = drop fst
+                  anon0_alt_data
+                MyFalse fst snd = 
+                  let _ = dup fst
+                  let _ = dup snd
+                  let anon1_alt_data = snd
+                  let _ = drop fst
+                  let _ = drop snd
+                  anon1_alt_data
+        |]
+            >>= IR.ann2Class def
+            >>= IR.class2Poly def
+    undropped `shouldPassAs` dropped
+    -- print $ runPass undropped
