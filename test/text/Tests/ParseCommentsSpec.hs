da2c466228234c20764590589af4aff00a2b97d9
j-hui <j-hui@cs.columbia.edu>
2022-04-18 12:54:58 -0400

    Add some more complex tests for weirdo comments

--- test/text/Tests/ParseCommentsSpec.hs
+++ test/text/Tests/ParseCommentsSpec.hs
@@ -46,6 +46,16 @@ spec = do
                 after 10 , (cout : & Int) <- 96
                 wait (cout : & Int)
             |]
+          , [here|
+
+              /* hello */
+
+              /* this is /* a nested */ comment */
+
+              main ( cout : & Int ) -> () =
+                after 10 , (cout : & Int) <- 96
+                wait (cout : & Int)
+            |]
           ]
     mapM_ (shouldPass . parseProgram) inputs
 
@@ -86,6 +96,16 @@ spec = do
 
 
             |]
+          , [here|
+              main ( cout : & Int ) -> () =
+                after 10 , (cout : & Int) <- 96
+                wait (cout : & Int)
+
+              /* hello */
+
+              /* this is /* a nested */ comment */
+
+            |]
           , "main cout = \n wait cout"
           ]
     mapM_ (shouldPass . parseProgram) inputs
@@ -109,6 +129,34 @@ spec = do
                 after 10 , (cout : & Int) <- 96
                   // a comment here
                 wait (cout : & Int)
+            |]
+          , [here|
+              /* this block comment bleeds
+              into the program */main ( cout : & Int ) -> () =
+                after 10 , (cout : & Int) <- 96
+                  // a comment here
+                wait (cout : & Int)
+              f x = x
+            |]
+          , [here|
+              /*{*/main/*s*/(/*p*/cout/*l*/:/*i*/&/*c*/Int/*e*/)/*d*/->/* */()/*!*/=/*}*/
+                after 10 , (cout : & Int) <- 96
+                wait (cout : & Int)
+            |]
+          , [here|
+              main ( cout : & Int ) -> () =
+              /* using comments to */  after 10 , (cout : & Int) <- 96
+                      /* is ok */
+                                       wait cout /* even */
+                /* leading */          wait (cout : & Int)  /* comments */
+              f x = x
+            |]
+          , [here|
+              main ( cout : & Int ) -> () = /* similar */  after 10 , (cout : & Int) <- 96
+                 /* idea */
+                                                           wait cout /* with blocks */
+                    /* started on */                       wait (cout : & Int) /* the same line */
+              f x = x
             |]
           ]
     mapM_ (shouldPass . parseProgram) inputs
