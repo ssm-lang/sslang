25b2c645ffb1b64f387cd0c5b5d619fdc6e1f6c4
John Hui <j-hui@cs.columbia.edu>
2022-04-20 01:40:29 -0400

    Support stray || inserted due to leading comments (#89)
    * Support stray || inserted due to leading comments
    
    Fix #79
    
    * Add test suite for (not) parsing comments

--- test/text/Tests/ScanCommentsSpec.hs
+++ test/text/Tests/ScanCommentsSpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Tests.ScanCommentsSpec where
 
 import           Sslang.Test
@@ -13,3 +14,27 @@ spec = do
   it "scans tokens before single-line comments" $ do
     scanTokenTypes "42 // no" `shouldProduce` [TInteger 42]
     scanTokenTypes "24// no" `shouldProduce` [TInteger 24]
+
+  it "supports leading comments" $ do
+    let input = [here|
+
+        // hello
+
+        42
+
+        |]
+        -- The leading TDBar is perhaps unexpected, but is tolerated by our
+        -- parser.
+        output = [TDBar, TInteger 42]
+    scanTokenTypes input `shouldProduce` output
+
+  it "supports trailing comments" $ do
+    let input = [here|
+
+        42
+
+        // hello
+
+        |]
+        output = [TInteger 42]
+    scanTokenTypes input `shouldProduce` output
