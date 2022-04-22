bb2fba2593a315a4a692cb4fa046d1f785835af8
EmilySillars <emsillars@gmail.com>
2022-04-21 23:05:35 -0400

    no conditional ops

--- regression-tests/testEmily.sh
+++
@@ -1,6 +0,0 @@
-###### Testing blink2
-stack exec sslc -- tests/emily.ssl > emily.c
-[]cc -O -g -Wall -Wno-unused-label -pedantic -std=c99 -I ../lib/ssm/include -I out -c -o emily.o emily.c
-cc -O -g -Wall -Wno-unused-label -pedantic -std=c99 -I ../lib/ssm/include -I out -c -o out/emily-main.o tests/emily-main.c
-cc -g -L ../lib/ssm/build -o emily emily.o emily-main.o -lssm
-emily
