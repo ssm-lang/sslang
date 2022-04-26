638eaa0cc134b174117c1e10b911231b51016269
j-hui <j-hui@cs.columbia.edu>
2022-04-26 12:38:33 -0400

    Use flush stdout when writing

--- lib/ssm/platform/posix/src/posix-io.c
+++ lib/ssm/platform/posix/src/posix-io.c
@@ -16,7 +16,9 @@ static void step_stdout_handler(ssm_act_t *act) {
     return;
   case 1:;
     char c = ssm_unmarshal(ssm_deref(cont->ssm_stdout));
-    putchar(c);
+    write(1, &c, 1);
+    // putchar(c);
+    fsync(1);
     return;
   }
   ssm_leave(&cont->act, sizeof(stdout_handler_act_t));
