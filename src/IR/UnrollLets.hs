8823c605abfa469bf3155d9abd4e911c4208ebf7
j-hui <j-hui@cs.columbia.edu>
2022-08-24 18:39:48 -0400

    Wip, something to do with proxies

--- src/IR/UnrollLets.hs
+++ src/IR/UnrollLets.hs
@@ -13,22 +13,26 @@ import           Data.Generics.Aliases          ( mkT )
 import           Data.Generics.Schemes          ( everywhere )
 import qualified Data.Set                      as S
 
+import Data.Data (Proxy (..))
+
+import Debug.Trace
+
 unrollLets :: Data t => I.Program t -> Compiler.Pass (I.Program t)
 unrollLets p = return
   $ p { I.programDefs = everywhere (mkT unrollLetExpr) $ I.programDefs p }
 
-unrollLetExpr :: I.Expr I.Type -> I.Expr I.Type
+unrollLetExpr :: Data t => I.Expr t -> I.Expr t
 unrollLetExpr (I.Let ds b t) = foldr ilet b $ segmentDefs ds
   where ilet d' b' = I.Let d' b' t
 unrollLetExpr e = e
 
-segmentDefs :: [(I.Binder, I.Expr I.Type)] -> [[(I.Binder, I.Expr I.Type)]]
+segmentDefs :: [(I.Binder, I.Expr t)] -> [[(I.Binder, I.Expr t)]]
 segmentDefs = reverse . map reverse . segment [] . reverse
  where
   segment acc [] = acc
   segment acc (d@(Just v, _) : ds)
-    | v `S.member` precedingFreeVars ds = pushSegment acc d `segment` ds
-    | otherwise                         = newSegment acc d `segment` ds
+    | v `S.member` precedingFreeVars ds = trace ("not segmenting " ++ show v) $ pushSegment acc d `segment` ds
+    | otherwise                         = trace ("segmenting " ++ show v) $ newSegment acc d `segment` ds
   segment acc (d : ds) = segment (pushSegment acc d) ds
 
   precedingFreeVars = S.unions . fmap (freeVars . snd)
