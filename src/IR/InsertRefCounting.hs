2a0a1ebbc2b41d9f095b3a01e880ed2e2f640298
Stephen A. Edwards <sedwards@cs.columbia.edu>
2022-09-21 17:34:11 -0400

    More documentation

--- src/IR/InsertRefCounting.hs
+++ src/IR/InsertRefCounting.hs
@@ -63,6 +63,8 @@ import qualified IR.Types                      as I
 -- $external
 
 -- | Insert dup and drop primitives throughout a program
+--
+-- Applies `insertTop` to the program's definitions
 insertRefCounting :: I.Program I.Type -> Compiler.Pass (I.Program I.Type)
 insertRefCounting program = (`evalStateT` 0) $ do
   let programDefs = I.programDefs program
@@ -77,34 +79,133 @@ insertRefCounting program = (`evalStateT` 0) $ do
 --
 -- $internal
 
+
+-- | Monad for creating fresh variables: add an Int to the pass
 type Fresh = StateT Int Compiler.Pass
 
--- | Get a fresh variable name
+-- | Create a fresh variable name with the given suffix
+--
+-- Creates a new variable ID of the form "anon1," "anon2,"
+-- etc. followed by the supplied suffix
 getFresh :: String -> Fresh I.VarId
 getFresh str = do
   curCount <- get
   modify (+ 1)
   return $ fromString $ ("anon" <> show curCount) ++ str
 
--- | Make a drop primitive with unit type
-makeDrop :: I.Expr I.Type -> I.Expr I.Type -> I.Expr I.Type
-makeDrop r e = I.Prim I.Drop [e, r] I.Unit
-
--- | Make a dup primitive with actual type
-makeDup :: I.Expr I.Type -> I.Expr I.Type
+-- | Make a dup primitive that returns the type of its argument
+makeDup :: I.Expr I.Type -- ^ The variable to duplicate and return
+        -> I.Expr I.Type -- ^ The @dup@ call
 makeDup e = I.Prim I.Dup [e] $ I.extract e
+
+-- | Make a drop primitive with unit type
+makeDrop :: I.Expr I.Type -- ^ The expression to evaluate and return
+         -> I.Expr I.Type -- ^ The variable to drop afterwards
+         -> I.Expr I.Type -- ^ The @drop@ call
+makeDrop r e = I.Prim I.Drop [e, r] I.Unit
 
 
 
 -- $internal
 
--- | Insert referencing counting for top-level expressions.
+-- | Insert referencing counting for top-level expressions
+--
+-- Applies `insertExpr` to a top-level delcaration
 insertTop :: (I.VarId, I.Expr I.Type) -> Fresh (I.VarId, I.Expr I.Type)
 insertTop (var, expr) = (var, ) <$> insertExpr expr
 
--- | Insert reference counting into expressions.
+{- | Insert reference counting into an expression
+
+This is the main workhorse of this module.
+
+* __Literals__ are unchanged, e.g., @42@ -> @42@
+
+* __Data constructors__ are unchanged, e.g., @True@ -> @True@ because they
+  are functions whose results are referenced
+  
+* A __variable reference__ becomes a call to dup because it introduces
+  another reference to the named object,
+
+> v
+
+becomes
+
+> dup v
+
+* __Application__ inserts dups and drops on both the function being
+  applied and its argument, e.g., @foo x@ -> @(dup foo) (dup x)@
+
+* __Primitive__ function application inserts dups and drops on its
+  arguments, e.g.,
+
+> (+) x y
+
+becomes
+
+> (+) (dup x) (dup y)
+
+* __Let__ introduces new names whose values are dropped after the body
+  is evaluated; @let _ =@ are given names so they can be dropped.
+
+
+* Nested __Lambda__ expressions are handled by collecting them into a
+  single expression with multiple arguments, adding dups and drops to the body,
+  and adding drops around the body for each argument.
+
+> add a b = a + b
+
+desugars to
+
+> add = fun a (fun b (a + b))
+
+becomes
+
+@
+add = fun a (
+        fun b (
+           drop (
+              drop (
+                (dup a) + (dup b)
+              ) b
+           ) a
+@
+
+* __Matches__ that operate on a variable are modified by inserting
+ dups and drops into the arms (but not the scrutinee)
+
+* __Matches__ that scrutinize an expression lift the scrutinee
+  into a @let@ then insert dups and drops on the whole thing
+
+@
+  match e
+    alt1 =
+    alt2 =
+@
+
+becomes
+
+@
+  let anon42_scrutinee = e
+  match s
+    alt1 =
+    alt2 =
+@
+
+-}
+
+--
 insertExpr :: I.Expr I.Type -> Fresh (I.Expr I.Type)
+
+insertExpr dcon@I.Data{}     = return dcon
+
+insertExpr lit@I.Lit{}       = return lit
+
+insertExpr var@I.Var{}       = return $ makeDup var
+
 insertExpr (I.App f x t) = I.App <$> insertExpr f <*> insertExpr x <*> pure t
+
+insertExpr (I.Prim p es typ) = I.Prim p <$> mapM insertExpr es <*> pure typ
+
 insertExpr (I.Let bins expr typ) = do
   bins' <- forM bins droppedBinder
   expr' <- insertExpr expr
@@ -118,10 +219,8 @@ insertExpr (I.Let bins expr typ) = do
   droppedBinder (v, d) = do
     d' <- insertExpr d
     return (v, d')
+
 insertExpr lam@(I.Lambda _ _ typ) = do
-  -- Handle nested lambdas by collecting them into a single expression
-  -- with multiple arguments, adding a local result variable,
-  -- drops for each of the arguments, and return value
   let (args    , body) = I.unfoldLambda lam
       (argTypes, _   ) = I.unfoldArrow typ
   args' <- forM args $ maybe (getFresh "_arg") return -- handle _ arguments
@@ -131,35 +230,14 @@ insertExpr lam@(I.Lambda _ _ typ) = do
   return $ I.foldLambda typedArgs $ foldr makeDrop body' argVars
 
 insertExpr (I.Match v@I.Var{} alts typ) = do
-  -- Note that we do not need to do anything for the scrutinee;
-  -- we just recurse on the alts.
   alts' <- forM alts insertAlt
   return $ I.Match v alts' typ
+  
 insertExpr (I.Match scrutExpr alts typ) = do
-  -- Desugar scrutinee into a variable so we can duplicate it as appropriate
-  --
-  -- @@
-  --   match e
-  --     alt1 =
-  --     alt2 =
-  -- @@
-  --
-  -- into
-  --
-  -- @@
-  --   let s = e
-  --   match s
-  --     alt1 =
-  --     alt2 =
-  -- @@
   scrutVar <- getFresh "_scrutinee"
   insertExpr $ I.Let [(Just scrutVar, scrutExpr)]
                      (I.Match (I.Var scrutVar $ I.extract scrutExpr) alts typ)
                      typ
-insertExpr (I.Prim p es typ) = I.Prim p <$> mapM insertExpr es <*> pure typ
-insertExpr var@I.Var{}       = return $ makeDup var
-insertExpr dcon@I.Data{}     = return dcon
-insertExpr lit@I.Lit{}       = return lit
 
 -- | Insert ref counting into pattern match arms.
 insertAlt :: (I.Alt, I.Expr I.Type) -> Fresh (I.Alt, I.Expr I.Type)
