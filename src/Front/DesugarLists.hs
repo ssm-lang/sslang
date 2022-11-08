a0185ac2192941d2e9453a8f9b82a77897ae037c
Max Acebal <maxacebal@gmail.com>
2022-11-08 15:26:09 -0500

    DesugarLists partial implementation

--- src/Front/DesugarLists.hs
+++ src/Front/DesugarLists.hs
@@ -1,29 +1,37 @@
 -- | Desugar ListExpr nodes into App nodes
 module Front.DesugarLists
-  ( desugarLists
-  ) where
+  ( desugarLists,
+  )
+where
 
-import qualified Common.Compiler               as Compiler
-import           Front.Ast                      ( Definition(..)
-                                                , Expr(..)
-                                                , Program(..)
-                                                , TopDef(..)
-                                                )
+import qualified Common.Compiler as Compiler
+import Front.Ast as A
+import Common.Identifiers as I
 
 -- | Desugar ListExpr nodes inside of an AST 'Program'.
 desugarLists :: Program -> Compiler.Pass Program
-desugarLists (Program decls) = return $ Program $ desugarTop <$> decls
- where
-  desugarTop (TopDef d) = TopDef $ desugarDef d
-  desugarTop t          = t
+desugarLists (Program decls) = return $ Program $ desugarTop <$> decls --test
+  where
+    desugarTop (TopDef d) = TopDef $ desugarDef d
+    desugarTop t = t
+    -- test = Compiler.unexpected $ (show $ (A.foldApp) (A.Id (I.Identifier "Cons")) [(Lit (LitInt 1)), (Lit (LitInt 2))]) 
+    desugarDef (DefFn v bs t e) = DefFn v bs t $ desugarExpr e
+    desugarDef (DefPat b e) = DefPat b $ desugarExpr e
 
-  desugarDef (DefFn v bs t e) = DefFn v bs t $ desugarExpr e
-  desugarDef (DefPat b e    ) = DefPat b $ desugarExpr e
-
--- | Transform a node of type ListExpr into a node of type App 
+-- | Transform a node of type ListExpr into a node of type App
 -- For ex, (ListExpr [1, 2, 3]) turns into
--- App (App (Id "Cons") (Lit (LitInt 1) )) 
---     (App (App (Id "Cons") (Lit (LitInt 2))) 
+-- App (App (Id "Cons") (Lit (LitInt 1) ))
+--     (App (App (Id "Cons") (Lit (LitInt 2)))
 --          (App (App (Id "Cons") (Lit (LitInt 3))) (id "Nil")))
+
+appidhelper:: A.ListExpr -> A.ListExpr
+appidhelper [] = [(A.Id (I.Identifier "App")) (A.Id (I.Identifier "Cons")) (A.Id (I.Identifier "Nil"))]
+appidhelper (h:t) = A.Id (I.Identifier "App") (A.Id (I.Identifier "Cons")) h : appidhelper t
+  
+--[(A.Id (I.Identifier "App")) (A.Id (I.Identifier "Cons")) | i <- a] 
 desugarExpr :: Expr -> Expr
-desugarExpr e = e -- TODO: actually implement transformation
+--desugarExpr _ = Compiler.unexpected $ (show (A.Lit(A.LitInt 5)))
+desugarExpr (A.ListExpr es) = A.foldApp h t
+where
+  (h : t) = appidhelper es
+
