bc26a7d5a9332bf0d08171939ac9209f8c68c189
EmilySillars <emsillars@gmail.com>
2022-11-10 10:02:03 -0500

    add dev hints

--- src/Front/DesugarLists.hs
+++ src/Front/DesugarLists.hs
@@ -1,6 +1,6 @@
 -- | Desugar ListExpr nodes into App nodes
 module Front.DesugarLists
-  ( desugarLists,
+  ( desugarLists
   )
 where
 
@@ -24,14 +24,31 @@ desugarLists (Program decls) = return $ Program $ desugarTop <$> decls --test
 --     (App (App (Id "Cons") (Lit (LitInt 2)))
 --          (App (App (Id "Cons") (Lit (LitInt 3))) (id "Nil")))
 
+{-
 appidhelper:: A.ListExpr -> A.ListExpr
 appidhelper [] = [(A.Id (I.Identifier "App")) (A.Id (I.Identifier "Cons")) (A.Id (I.Identifier "Nil"))]
 appidhelper (h:t) = A.Id (I.Identifier "App") (A.Id (I.Identifier "Cons")) h : appidhelper t
+
+ListExpr is not a type, it's a data constructor.
+Data constructors return an instance of a type. 
+ListExpr is a data constructor that returns an instance of type Expr.
+It's not possible to have a function that takes in
+something of type ListExpr and returns something of type ListExpr, because a ListExpr is not a type.
+It is possible to have a function that takes in 
+something of type Expr and returns something of type Expr!
+Then you can special case on instance of ListExpr.
+I think this is what you wanted to do.
+-}
+
+appidhelper:: Expr -> Expr
+appidhelper (ListExpr []) = [(A.Id (I.Identifier "App")) (A.Id (I.Identifier "Cons")) (A.Id (I.Identifier "Nil"))]
+appidhelper (ListExpr (h:t)) = A.Id (I.Identifier "App") (A.Id (I.Identifier "Cons")) h : appidhelper t
+appidhelper e = e -- if it's not a listExpr instance, don't do anything
+
   
 --[(A.Id (I.Identifier "App")) (A.Id (I.Identifier "Cons")) | i <- a] 
 desugarExpr :: Expr -> Expr
 --desugarExpr _ = Compiler.unexpected $ (show (A.Lit(A.LitInt 5)))
 desugarExpr (A.ListExpr es) = A.foldApp h t
-where
-  (h : t) = appidhelper es
-
+  where (h : t) = appidhelper es
+desugarExpr e = e -- if it's not a listExpr instance, don't do anything
