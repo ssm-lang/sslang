9b32caa7db4d02dbe2c98cd366337dd409467b04
Eric Feng <ef2648@columbia.edu>
2022-11-30 16:56:39 -0500

    Understanding notes + plan

--- src/IR/Simplify.hs
+++ src/IR/Simplify.hs
@@ -32,14 +32,15 @@ import qualified Data.Map                      as M
 
 -- | Occurrence Information for each binding
 data OccInfo = Dead
-             | LoopBreaker
+             | LoopBreaker -- TBD
              | OnceSafe
              | MultiSafe
              | OnceUnsafe
              | MultiUnsafe
+             | Never
 
 -- | Simplifier Environment
-data SimplCtx = SimplCtx
+data SimplEnv = SimplEnv
   { occInfo :: M.Map I.VarId OccInfo
   {- ^ 'occInfo' maps an identifier to its occurence category -}
   , runs    :: Int
@@ -47,17 +48,25 @@ data SimplCtx = SimplCtx
   }
 
 -- | Simplifier Monad
-newtype SimplFn a = SimplFn (StateT SimplCtx Compiler.Pass a)
-  deriving Functor                      via (StateT SimplCtx Compiler.Pass)
-  deriving Applicative                  via (StateT SimplCtx Compiler.Pass)
-  deriving Monad                        via (StateT SimplCtx Compiler.Pass)
-  deriving MonadFail                    via (StateT SimplCtx Compiler.Pass)
-  deriving (MonadError Compiler.Error)  via (StateT SimplCtx Compiler.Pass)
-  deriving (MonadState SimplCtx)         via (StateT SimplCtx Compiler.Pass)
+newtype SimplFn a = SimplFn (StateT SimplEnv Compiler.Pass a)
+  deriving Functor                      via (StateT SimplEnv Compiler.Pass)
+  deriving Applicative                  via (StateT SimplEnv Compiler.Pass)
+  deriving Monad                        via (StateT SimplEnv Compiler.Pass)
+  deriving MonadFail                    via (StateT SimplEnv Compiler.Pass)
+  deriving (MonadError Compiler.Error)  via (StateT SimplEnv Compiler.Pass)
+  deriving (MonadState SimplEnv)         via (StateT SimplEnv Compiler.Pass)
 
 -- | Run a SimplFn computation.
 runSimplFn :: SimplFn a -> Compiler.Pass a
-runSimplFn (SimplFn m) = evalStateT m SimplCtx { occInfo = M.empty, runs = 0 }
+--evalStateT runs function in context and returns final value, without context
+  -- take state transformer monad and an initial state, and gives the initial state to the transformer monad. (and the transformer monad holds our monadic program)
+      -- runSimplFun takes in the Compiler.Pass wrapped inside a SimplFn
+      -- at the moment of evalStateT taking in m and SimplCtx, m has Compiler.Pass (from do) but not SimplCtx yet
+        -- but so you have m that doesn't have SimplCtx yet?? (IDK) -- look at stackoverflow post
+          -- state transformer: function that takes in state and returns final value (Compiler.Pass)
+  -- m is state transformer monad (result of do inside simplifyProgram)
+  -- SimplCtx is initial state
+runSimplFn (SimplFn m) = evalStateT m SimplEnv { occInfo = M.empty, runs = 0 } 
 
 -- | Add a binder to occInfo with category Dead by default
 addOccVar :: I.VarId -> SimplFn ()
@@ -74,10 +83,11 @@ Maps over top level definitions to create a new simplified Program
 Do we ever want to inline top-level definitions?
 -}
 simplifyProgram :: I.Program I.Type -> Compiler.Pass (I.Program I.Type)
-simplifyProgram p = runSimplFn $ do
+simplifyProgram p = runSimplFn $ do -- everything in do expression will know about simplifier environment
+                                    -- run this do expression and give it the knowledge of my simplifier environment
   let defs = I.programDefs p
   simplifiedProgramDefs <- mapM simplTop defs
-  return $ p { I.programDefs = simplifiedProgramDefs }
+  return $ p { I.programDefs = simplifiedProgramDefs } -- this whole do expression returns a Compiler.Pass
 
 -- | Simplify a top-level definition
 simplTop :: (I.VarId, I.Expr I.Type) -> SimplFn (I.VarId, I.Expr I.Type)
@@ -94,3 +104,11 @@ simplExpr e = do
   addOccVar "test"
   return e -- what a stub.
 
+{-
+thoughts:
+Decouple occurence analyzer from simplExpr
+
+have occurence analyzer fill in SimplEnv and just feed it into runSimplFn; 
+
+runSimplFn will purely care about inlining
+-}
