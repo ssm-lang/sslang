b22694f06f2f0abf270e1fd1a437f6bcf15a4de1
Anjali Smith <as6467@barnard.edu>
2022-11-30 00:22:13 -0500

    use everywhere in DesugarLists.hs to simplify the pass

--- src/Front/DesugarLists.hs
+++ src/Front/DesugarLists.hs
@@ -5,11 +5,12 @@ module Front.DesugarLists
 
 import qualified Common.Compiler               as Compiler
 import Common.Identifiers                       (Identifier(Identifier))
-import           Front.Ast                      ( Definition(..)
+import Front.Ast                                ( Definition(..)
                                                 , Expr(..)
                                                 , Program(..)
                                                 , TopDef(..)
                                                 )
+import Data.Generics                            ( mkT, everywhere )                                             
 
 -- | Desugar ListExpr nodes inside of an AST 'Program'.
 desugarLists :: Program -> Compiler.Pass Program
@@ -18,8 +19,8 @@ desugarLists (Program decls) = return $ Program $ desugarTop <$> decls
   desugarTop (TopDef d) = TopDef $ desugarDef d
   desugarTop t          = t
 
-  desugarDef (DefFn v bs t e) = DefFn v bs t $ desugarExpr e
-  desugarDef (DefPat b e    ) = DefPat b $ desugarExpr e
+  desugarDef (DefFn v bs t e) = DefFn v bs t $ everywhere (mkT desugarExpr) e
+  desugarDef (DefPat b e    ) = DefPat b $ everywhere (mkT desugarExpr) e
 
 -- | Transform a node of type ListExpr into a node of type App 
 -- For ex, (ListExpr [1, 2, 3]) turns into
