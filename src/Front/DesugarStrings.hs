fa01f44b58595ee5682b0d8bdbec0bd7d296c514
Anjali Smith <as6467@barnard.edu>
2022-12-02 14:31:47 -0500

    Format file

--- src/Front/DesugarStrings.hs
+++ src/Front/DesugarStrings.hs
@@ -4,15 +4,16 @@ module Front.DesugarStrings
   ) where
 
 import qualified Common.Compiler               as Compiler
+import           Data.Char                      ( ord )
+import           Data.Generics                  ( everywhere
+                                                , mkT
+                                                )
 import           Front.Ast                      ( Definition(..)
                                                 , Expr(..)
+                                                , Literal(..)
                                                 , Program(..)
                                                 , TopDef(..)
-                                                , Literal(..)
                                                 )
-import           Data.Char                      ( ord
-                                                )
-import Data.Generics                            ( mkT, everywhere )                                             
 
 -- | Desugar String Literal nodes inside of an AST 'Program'.
 desugarStrings :: Program -> Compiler.Pass Program
@@ -23,13 +24,13 @@ desugarStrings (Program decls) = return $ Program $ desugarTop <$> decls
 
   desugarDef (DefFn v bs t e) = DefFn v bs t $ everywhere (mkT desugarExpr) e
   desugarDef (DefPat b e    ) = DefPat b $ everywhere (mkT desugarExpr) e
-  
+
 
 -- | Transform a node of type LitString into a node of type ListExpr
 -- For ex, (Lit (LitString "abc")) turns into (ListExpr [97, 98, 99])
 desugarExpr :: Expr -> Expr
 desugarExpr (Lit (LitString s)) = ListExpr (convertList s)
-desugarExpr e = e 
+desugarExpr e                   = e
 
 convertList :: [Char] -> [Expr]
-convertList ls = [Lit (LitInt (toInteger i)) | i <- map ord ls]
+convertList ls = [ Lit (LitInt (toInteger i)) | i <- map ord ls ]
