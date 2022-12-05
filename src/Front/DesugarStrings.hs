cf862eee8445894948aa07e769f2926feffa2696
Anjali Smith <93302754+as0501@users.noreply.github.com>
2022-12-05 17:10:39 -0500

    Add string literal syntax

--- src/Front/DesugarStrings.hs
+++ src/Front/DesugarStrings.hs
@@ -4,8 +4,13 @@ module Front.DesugarStrings
   ) where
 
 import qualified Common.Compiler               as Compiler
+import           Data.Char                      ( ord )
+import           Data.Generics                  ( everywhere
+                                                , mkT
+                                                )
 import           Front.Ast                      ( Definition(..)
                                                 , Expr(..)
+                                                , Literal(..)
                                                 , Program(..)
                                                 , TopDef(..)
                                                 )
@@ -17,10 +22,13 @@ desugarStrings (Program decls) = return $ Program $ desugarTop <$> decls
   desugarTop (TopDef d) = TopDef $ desugarDef d
   desugarTop t          = t
 
-  desugarDef (DefFn v bs t e) = DefFn v bs t $ desugarExpr e
-  desugarDef (DefPat b e    ) = DefPat b $ desugarExpr e
+  desugarDef (DefFn v bs t e) = DefFn v bs t $ everywhere (mkT desugarExpr) e
+  desugarDef (DefPat b e    ) = DefPat b $ everywhere (mkT desugarExpr) e
+
 
 -- | Transform a node of type LitString into a node of type ListExpr
 -- For ex, (Lit (LitString "abc")) turns into (ListExpr [97, 98, 99])
 desugarExpr :: Expr -> Expr
-desugarExpr e = e -- TODO: actually implement transformation
+desugarExpr (Lit (LitString s)) = ListExpr (convertList s)
+  where convertList ls = [ Lit (LitInt (toInteger i)) | i <- map ord ls ]
+desugarExpr e = e
