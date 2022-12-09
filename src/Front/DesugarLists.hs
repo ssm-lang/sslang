4d018c12039bc5134166ba735a9e58192c3dd9d2
max-acebal <60718877+max-acebal@users.noreply.github.com>
2022-12-09 14:38:37 -0500

    Add list literal syntax (#119)
    * Broken List Parser
    
    * fix to main
    
    * Small chagnes to pattern match ListExpr
    
    * Todo add list with element implentation to Scope
    
    * Looping through elements of ListExpr in scope
    
    * Add todo message to LowerAst
    
    * compiler.unexpected in lowerAst
    
    * fix warnings and failing regression test
    
    * fix desugarExpr case, add newlines
    
    * remove unnecessary file
    
    * DesugarLists partial implementation
    
    * add dev hints
    
    * Desugaring Lists progress, no longer using foldapp
    
    * desugar lists with errors
    
    * Bypass matching on diff Exprs
    
    * Test cases passing
    
    * Added more meaningful tests to lists_sample
    
    * Fix warnings and style nits
    
    * fix imports
    
    Co-authored-by: EmilySillars <emsillars@gmail.com>

--- src/Front/DesugarLists.hs
+++ src/Front/DesugarLists.hs
@@ -4,7 +4,7 @@ module Front.DesugarLists
   ) where
 
 import qualified Common.Compiler               as Compiler
-import Common.Identifiers                       (Identifier(Identifier))
+import Common.Identifiers
 import Front.Ast                                ( Definition(..)
                                                 , Expr(..)
                                                 , Program(..)
@@ -14,23 +14,21 @@ import Data.Generics                            ( mkT, everywhere )
 
 -- | Desugar ListExpr nodes inside of an AST 'Program'.
 desugarLists :: Program -> Compiler.Pass Program
-desugarLists (Program decls) = return $ Program $ desugarTop <$> decls
- where
-  desugarTop (TopDef d) = TopDef $ desugarDef d
-  desugarTop t          = t
+desugarLists (Program decls) = return $ Program $ desugarTop <$> decls 
+  where
+    desugarTop (TopDef d) = TopDef $ desugarDef d
+    desugarTop t = t
+    desugarDef (DefFn v bs t e) = DefFn v bs t $ everywhere (mkT desugarExpr) e
+    desugarDef (DefPat b e    ) = DefPat b $ everywhere (mkT desugarExpr) e
 
-  desugarDef (DefFn v bs t e) = DefFn v bs t $ everywhere (mkT desugarExpr) e
-  desugarDef (DefPat b e    ) = DefPat b $ everywhere (mkT desugarExpr) e
-
--- | Transform a node of type ListExpr into a node of type App 
+-- | Transform a node of type ListExpr into a node of type App
 -- For ex, (ListExpr [1, 2, 3]) turns into
--- App (App (Id "Cons") (Lit (LitInt 1) )) 
---     (App (App (Id "Cons") (Lit (LitInt 2))) 
+-- App (App (Id "Cons") (Lit (LitInt 1) ))
+--     (App (App (Id "Cons") (Lit (LitInt 2)))
 --          (App (App (Id "Cons") (Lit (LitInt 3))) (id "Nil")))
 desugarExpr :: Expr -> Expr
-desugarExpr (ListExpr es) = func es
-desugarExpr e = e 
-
-func :: [Expr] -> Expr
-func [] = Id (Identifier "Nil")
-func t = foldr (Apply . Apply (Id (Identifier "Cons"))) (Id (Identifier "Nil")) t
+desugarExpr (ListExpr es) = helper es
+  where helper :: [Expr] -> Expr
+        helper [] = (Id nil)
+        helper (h:t) = Apply (Apply (Id cons) h) (helper t)
+desugarExpr e = e
