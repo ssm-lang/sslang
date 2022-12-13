43ae9294d10cd0fec6b64ab9c8b61a273aba7a4b
EmilySillars <emsillars@gmail.com>
2022-12-13 01:21:46 -0500

    make occurence info print out in an error

--- src/IR/Simplify.hs
+++ src/IR/Simplify.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use record patterns" #-}
 {- | Lift lambda definitions into the global scope.
 
 This pass is responsible for moving nested lambda definitions into the global
@@ -16,7 +18,10 @@ import qualified Common.Compiler               as Compiler
 import qualified IR.IR                         as I
 
 -- import           Control.Monad                  ( forM_ )
-import           Control.Monad.Except           ( MonadError(..) )
+-- import           Control.Monad                  ( forM_ )
+import           Control.Monad.Except           ( MonadError(..)
+                                                
+                                                )
 import           Control.Monad.State.Lazy       ( MonadState
                                                 , StateT(..)
                                                 , evalStateT
@@ -25,14 +30,25 @@ import           Control.Monad.State.Lazy       ( MonadState
                                                 -- , unless
                                                 )
 
-import           Data.Bifunctor                 ( first, second )
+import           Data.Bifunctor                 ( second )
 -- import           Data.List                      ( intersperse )
 import qualified Data.Map                      as M
-import qualified GHC.IO.Exception as Compiler
+-- import qualified GHC.IO.Exception              as Compiler
 -- import           Data.Maybe                     ( catMaybes )
 -- import qualified Data.Set                      as S
 
--- | Occurrence Information for each binding
+{-  | Occurrence Information for each binding
+
+Dead: Does not appear at all
+OnceSafe: Appears once, NOT inside a lambda
+MultiSafe: The binder occurs at most ONCE in each of several distinct case branches; 
+           NONE of these ocurrences is inside a lambda
+OnceUnsafe: Binder occurs exactly once, but inside a lambda. 
+Multisafe: Binder may occur many times, including inside lambdas. 
+           Variables exported from the module are also makred MultiUnsafe.
+LoopBreaker: Chosen to break dependency between mutually recursive defintions. 
+Never: Never inline; we use this to develop our inliner incrementally.
+-}
 data OccInfo = Dead
              | LoopBreaker -- TBD
              | OnceSafe
@@ -44,11 +60,14 @@ data OccInfo = Dead
 
 -- | Simplifier Environment
 data SimplEnv = SimplEnv
-  { occInfo :: M.Map I.VarId OccInfo
+  { occInfo      :: M.Map I.VarId OccInfo
   {- ^ 'occInfo' maps an identifier to its occurence category -}
-  , runs    :: Int
+  , runs         :: Int
   {- ^ 'runs' stores how many times the simplifier has run so far -}
-  } deriving (Show)
+  , insideLambda :: Bool
+  {- ^ 'insideLambda' whether ocurrence analyzer is currently inspecting a lambda -}
+  }
+  deriving Show
 
 -- | Simplifier Monad
 newtype SimplFn a = SimplFn (StateT SimplEnv Compiler.Pass a)
@@ -69,15 +88,28 @@ runSimplFn :: SimplFn a -> Compiler.Pass a
           -- state transformer: function that takes in state and returns final value (Compiler.Pass)
   -- m is state transformer monad (result of do inside simplifyProgram)
   -- SimplCtx is initial state
-runSimplFn (SimplFn m) = evalStateT m SimplEnv { occInfo = M.empty, runs = 0 }
+runSimplFn (SimplFn m) =
+  evalStateT m SimplEnv { occInfo = M.empty, runs = 0, insideLambda = False }
 
+-- | Update occInfo for the binder since we just spotted it
 updateOccVar :: I.VarId -> SimplFn ()
 updateOccVar binder = do
-  m <- gets occInfo
+  m          <- gets occInfo
+  onceunsafe <- gets insideLambda
   let m' = case M.lookup binder m of
-        Nothing   -> error ("UDPATE: We should already know about this binder " ++ show m ++"!")
-        Just Dead -> M.insert binder OnceSafe m -- we only handle oncesafe currently
-        _         -> M.insert binder Never m
+        Nothing ->
+          error
+            (  "UDPATE: We should already know about this binder "
+            ++ show m
+            ++ "!"
+            )
+        Just Dead -> do
+          -- we only handle OnceSafe currently
+          -- if we're inside a lambda, binder is NOT OnceSafe (in fact, it's OnceUnsafe...)
+          if onceunsafe
+            then M.insert binder Never m
+            else M.insert binder OnceSafe m
+        _ -> M.insert binder Never m
   modify $ \st -> st { occInfo = m' }
 
 -- | Add a binder to occInfo with category Dead by default
@@ -86,9 +118,21 @@ addOccVar binder = do
   m <- gets occInfo
   let m' = case M.lookup binder m of
         Nothing -> M.insert binder Dead m
-        _       -> error ("ADD: Should never have never seen this binder "++ show binder ++" before!")
+        _ ->
+          error
+            (  "ADD: Should never have seen this binder "
+            ++ show binder
+            ++ " before!"
+            )
   modify $ \st -> st { occInfo = m' }
 
+-- | Record that the ocurrence analyzer is looking inside a lambda
+recordEnteringLambda :: SimplFn ()
+recordEnteringLambda = modify $ \st -> st { insideLambda = True }
+
+-- | Record that the ocurrence analyzer is no longer looking inside a lambda
+recordExitingLambda :: SimplFn ()
+recordExitingLambda = modify $ \st -> st { insideLambda = False }
 
 
 {- | Entry-point to Simplifer.
@@ -100,15 +144,40 @@ simplifyProgram :: I.Program I.Type -> Compiler.Pass (I.Program I.Type)
 simplifyProgram p = runSimplFn $ do -- everything in do expression will know about simplifier environment
                                     -- run this do expression and give it the knowledge of my simplifier environment
   -- run the occurrence analyzer
-  z@(p',info) <- runOccAnal p
+  (_, info) <- runOccAnal p
+  -- fail and print out the results of the occurence analyzer
+  _ <- Compiler.unexpected $ show info
+  -- I should always have at least one top-level func, main.
+  -- let's see how many top level funcs I have!
+  -- Compiler.unexpected $ show $ topLevelFunc <$> defs
   let defs = I.programDefs p
   simplifiedProgramDefs <- mapM simplTop defs
-  Compiler.unexpected $ show info
   return $ p { I.programDefs = simplifiedProgramDefs } -- this whole do expression returns a Compiler.Pass
+
+{- | Check if defintion is a top-level function
+
+Every top level defintion is either
+1) a top level function of the form (VarId, Lambda v b t)
+2) a global variable of the form (VarId, some kind of expr that isn't a Lambda)
+-}
+-- topLevelFunc :: (I.VarId, I.Expr I.Type) -> Maybe I.VarId -- get rid of me someday
+-- topLevelFunc (v, I.Lambda _ _ _) = Just v 
+-- topLevelFunc _                   = Nothing
+
 
 -- | Simplify a top-level definition
 simplTop :: (I.VarId, I.Expr I.Type) -> SimplFn (I.VarId, I.Expr I.Type)
-simplTop (v, e) = (,) v <$> simplExpr e
+simplTop (v, e) = do
+  (,) v <$> simplExpr e
+
+{- | Recursively simplify IR expressions.
+
+Probably want more documentation here eventually.
+-}
+simplExpr :: I.Expr I.Type -> SimplFn (I.Expr I.Type)
+simplExpr e = do
+  --addOccVar "test" <- cursed
+  return e -- what a stub.
 
 {-
 data Program t = Program
@@ -158,40 +227,84 @@ y-}
 3) {}
 -}
 
+
+
+
 runOccAnal :: I.Program I.Type -> SimplFn (I.Program I.Type, String)
 runOccAnal p@I.Program { I.programDefs = defs } = do
   {-
   stack build
   cd regression-tests
   ./runtests.sh -k tests/a-inlining-ex-1.ssl
+  stack exec sslc -- --dump-ir tests/a-inlining-ex-1.ssl
+  stack exec sslc -- --dump-ir-typed tests/a-inlining-ex-1.ssl
+  stack exec sslc -- --dump-ir-lifted tests/a-inlining-ex-1.ssl
   -}
-  --let x = map (second occAnalExpr) defs
-  y <- snd$ second occAnalExpr (head defs)
+  -- a hacky way to see the occurence info for each top def
+  let getOccInfoForDef
+        :: (I.VarId, SimplFn (I.Expr I.Type, String)) -> SimplFn String
+      getOccInfoForDef (v, tpl) = do
+        (_, occinfo) <- tpl
+        pure
+          $  "START topdef "
+          ++ show v
+          ++ " has OccInfo: "
+          ++ show occinfo
+          ++ " END"
+
+  x <- mapM (getOccInfoForDef . second occAnalExpr) defs
+  --y <- snd $ second occAnalExpr (head defs)
   --error $ show x
  -- addOccVar "yo"
-  m <- gets occInfo
+ -- m <- gets occInfo
  -- error (show m)
-  return (p,show y)
+  return (p, show x)
 
--- plan: recurse first on rhs then go into body
+
+-- | Run the Ocurrence Analyzer over an IR expression node
 occAnalExpr :: I.Expr I.Type -> SimplFn (I.Expr I.Type, String)
-occAnalExpr (I.Let binders body t) = do
+
+-- let expressions
+occAnalExpr l@(I.Let binders body _) = do
   mapM_
     (\(binder, rhs) -> case binder of
       (Just nm) -> do
         addOccVar nm
-       -- _ <- occAnalExpr rhs
+        _ <- occAnalExpr rhs
         pure ()
       Nothing -> pure ()
     )
     binders
-  --_ <- occAnalExpr body
+  _ <- occAnalExpr body
   m <- gets occInfo
-  --error (show m)
-  pure ((I.Let binders body t),show m)
+  pure (l, show m)
 
-occAnalExpr var@(I.Var v _) = do updateOccVar v
-                                 return (var,"other case")
+-- variables
+occAnalExpr var@(I.Var v _) = do
+  updateOccVar v
+  m <- gets occInfo
+  return (var, show m)
+
+-- lambdas
+occAnalExpr l@(I.Lambda Nothing b _) = do
+  recordEnteringLambda
+  _ <- occAnalExpr b
+  recordExitingLambda
+  m <- gets occInfo
+  pure (l, show m)
+occAnalExpr l@(I.Lambda (Just binder) b _) = do
+  recordEnteringLambda
+  addOccVar binder
+  _ <- occAnalExpr b
+  recordExitingLambda
+  m <- gets occInfo
+  pure (l, show m)
+
+-- everything else
+occAnalExpr e = do
+  m <- gets occInfo
+  pure (e, show m)
+
 {-
   = Var VarId t
   {- ^ @Var n t@ is a variable named @n@ of type @t@. -}
@@ -222,9 +335,9 @@ occAnalExpr var@(I.Var v _) = do updateOccVar v
   of type @t@.
   -}
   -}
-occAnalExpr l@(I.Lambda v b t) = pure (l, "im a lambda")
+
 -- --occAnalExpr (I.Var varId) = -- first time you see it, dead -> oncesafe
-occAnalExpr e = pure (e, "hello")
+
 
 
 {-
@@ -266,18 +379,6 @@ y
 --   -}
 -- -}
 
-
-
-{- | Recursively simplify IR expressions.
-
-Probably want more documentation here eventually.
-
--}
-simplExpr :: I.Expr I.Type -> SimplFn (I.Expr I.Type)
-simplExpr e = do
-  --addOccVar "test"
-  return e -- what a stub.
-
 {-
 thoughts:
 Decouple occurence analyzer from simplExpr
@@ -317,3 +418,15 @@ y
 Plan:
 add specialFunc
 -}
+
+{- | Given the RHS of a top level function, return first expr inside it that isn't a Lambda
+
+For each curried lambda expr I find, add binders to occurence info.
+Basically, "swallow up" the arguments to my top-level function.
+-}
+-- swallowArgs :: I.Expr I.Type -> SimplFn (I.Expr I.Type)
+-- swallowArgs (I.Lambda Nothing b _) = swallowArgs b
+-- swallowArgs (I.Lambda (Just arg) b _)= do
+--   addOccVar arg 
+--   swallowArgs b
+-- swallowArgs e = pure e
