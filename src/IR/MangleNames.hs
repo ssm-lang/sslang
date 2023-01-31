a33427b142ea4fda5aa25327a1f881aae1856b79
j-hui <j-hui@cs.columbia.edu>
2023-01-31 12:02:11 -0500

    Fix things after different binder structure

--- src/IR/MangleNames.hs
+++ src/IR/MangleNames.hs
@@ -1,36 +1,27 @@
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
-module IR.MangleNames
-  ( mangleProgram
-  , pickId
-  ) where
 
-import qualified Common.Compiler               as Compiler
-import           Common.Identifiers             ( Identifiable(..)
-                                                , IsString(..)
-                                                )
-import qualified IR.IR                         as I
+module IR.MangleNames (mangleProgram, pickId) where
 
-import           Control.Monad.Except           ( MonadError(..) )
-import           Control.Monad.State            ( MonadState
-                                                , StateT(..)
-                                                , evalStateT
-                                                , gets
-                                                , modify
-                                                )
-import           Data.Bifunctor                 ( Bifunctor(..) )
-import qualified Data.Map                      as M
-import           Data.Maybe                     ( catMaybes
-                                                , fromJust
-                                                )
-import qualified Data.Set                      as S
+import qualified Common.Compiler as Compiler
+import Common.Identifiers (Identifiable (..), IsString (..))
+import qualified IR.IR as I
+
+import Control.Monad.Except (MonadError (..))
+import Control.Monad.State (MonadState, StateT (..), evalStateT, gets, modify)
+import Data.Bifunctor (Bifunctor (..))
+import qualified Data.Map as M
+import Data.Maybe (catMaybes, fromJust)
+import qualified Data.Set as S
+
 
 normalize :: I.VarId -> I.VarId
 normalize = fromString . map m . ident
  where
   m '\'' = '_'
-  m a    = a
+  m a = a
+
 
 pickId :: M.Map I.VarId I.VarId -> I.VarId -> (I.VarId, I.VarId)
 pickId globals v =
@@ -39,42 +30,47 @@ pickId globals v =
   pick :: Int -> (I.VarId, I.VarId)
   pick i =
     let v' = normalize (v <> "___" <> fromString (show i))
-    in  if alreadyInUse v' then pick (i + 1) else (v, v')
+     in if alreadyInUse v' then pick (i + 1) else (v, v')
 
   alreadyInUse :: I.VarId -> Bool
   alreadyInUse v' = M.member v' globals
 
 
 data MangleCtx = MangleCtx
-  { localScope  :: M.Map I.VarId I.VarId
+  { localScope :: M.Map I.VarId I.VarId
   , globalScope :: M.Map I.VarId I.VarId
   }
 
+
 -- | Mangling monad
 newtype Mangle a = Mangle (StateT MangleCtx Compiler.Pass a)
-  deriving Functor                      via (StateT MangleCtx Compiler.Pass)
-  deriving Applicative                  via (StateT MangleCtx Compiler.Pass)
-  deriving Monad                        via (StateT MangleCtx Compiler.Pass)
-  deriving MonadFail                    via (StateT MangleCtx Compiler.Pass)
-  deriving (MonadError Compiler.Error)  via (StateT MangleCtx Compiler.Pass)
-  deriving (MonadState MangleCtx)       via (StateT MangleCtx Compiler.Pass)
+  deriving (Functor) via (StateT MangleCtx Compiler.Pass)
+  deriving (Applicative) via (StateT MangleCtx Compiler.Pass)
+  deriving (Monad) via (StateT MangleCtx Compiler.Pass)
+  deriving (MonadFail) via (StateT MangleCtx Compiler.Pass)
+  deriving (MonadError Compiler.Error) via (StateT MangleCtx Compiler.Pass)
+  deriving (MonadState MangleCtx) via (StateT MangleCtx Compiler.Pass)
+
 
 runMangle :: Mangle a -> Compiler.Pass a
 runMangle (Mangle m) =
-  evalStateT m MangleCtx { localScope = M.empty, globalScope = M.empty }
+  evalStateT m MangleCtx{localScope = M.empty, globalScope = M.empty}
+
 
 withLocals :: [(I.VarId, I.VarId)] -> Mangle a -> Mangle a
 withLocals vs m = do
   locals <- gets localScope
-  modify $ \st -> st { localScope = M.fromList vs `M.union` locals }
+  modify $ \st -> st{localScope = M.fromList vs `M.union` locals}
   a <- m
-  modify $ \st -> st { localScope = locals }
+  modify $ \st -> st{localScope = locals}
   return a
+
 
 tellGlobals :: [(I.VarId, I.VarId)] -> Mangle ()
 tellGlobals vs = do
   let vs' = map (\(a, b) -> (b, a)) vs
-  modify $ \st -> st { globalScope = M.fromList vs' `M.union` globalScope st }
+  modify $ \st -> st{globalScope = M.fromList vs' `M.union` globalScope st}
+
 
 withMangled :: [I.Binder] -> Mangle a -> Mangle ([I.Binder], a)
 withMangled vs m = do
@@ -92,7 +88,8 @@ withMangled vs m = do
 
   toBinder :: Maybe (I.VarId, I.VarId) -> I.Binder
   toBinder (Just (_, v')) = Just v'
-  toBinder Nothing        = Nothing
+  toBinder Nothing = Nothing
+
 
 mangleProgram :: I.Program I.Type -> Compiler.Pass (I.Program I.Type)
 mangleProgram p = runMangle $ do
@@ -101,10 +98,13 @@ mangleProgram p = runMangle $ do
   withLocals eds $ do
     let (tvs, tes) = unzip $ map (first Just) $ I.programDefs p
     (tvs', tes') <- withMangled tvs $ mapM mangleExpr tes
-    names        <- gets globalScope
-    return p { I.programDefs = zip (map fromJust tvs') tes'
-             , I.varNames    = names `M.union` I.varNames p
-             }
+    names <- gets globalScope
+    return
+      p
+        { I.programDefs = zip (map fromJust tvs') tes'
+        , I.varNames = names `M.union` I.varNames p
+        }
+
 
 mangleExpr :: I.Expr I.Type -> Mangle (I.Expr I.Type)
 mangleExpr (I.Var i t) = do
@@ -112,10 +112,10 @@ mangleExpr (I.Var i t) = do
   I.Var <$> maybe err return mv <*> pure t
  where
   err = Compiler.unexpected $ "MangleNames: Could not find I.Var " <> show i
-mangleExpr e@(I.Data _ _                   ) = return e
-mangleExpr e@(I.Lit  _ _                   ) = return e
+mangleExpr e@(I.Data _ _) = return e
+mangleExpr e@(I.Lit _ _) = return e
 mangleExpr (I.App f a t) = I.App <$> mangleExpr f <*> mangleExpr a <*> pure t
-mangleExpr (  I.Let (unzip -> (vs, ds)) b t) = do
+mangleExpr (I.Let (unzip -> (vs, ds)) b t) = do
   (vs', (ds', b')) <-
     withMangled vs $ (,) <$> mapM mangleExpr ds <*> mangleExpr b
   return $ I.Let (zip vs' ds') b' t
@@ -123,19 +123,23 @@ mangleExpr (I.Lambda i b t) = do
   ([i'], b') <- withMangled [i] $ mangleExpr b
   return $ I.Lambda i' b' t
 mangleExpr (I.Match s as t) = do
-  s'  <- mangleExpr s
+  s' <- mangleExpr s
   as' <- mapM mangleArm as
   return $ I.Match s' as' t
 mangleExpr (I.Prim p es t) = I.Prim p <$> mapM mangleExpr es <*> pure t
+mangleExpr e@I.Exception{} = return e
+
 
 mangleArm :: (I.Alt, I.Expr I.Type) -> Mangle (I.Alt, I.Expr I.Type)
 mangleArm (alt, ex) = do
-  snd <$> withMangled (map Just $ S.toList $ I.altBinders alt)
-                      ((,) <$> mangleAlt alt <*> mangleExpr ex)
+  snd
+    <$> withMangled
+      (map Just $ S.toList $ I.altBinders alt)
+      ((,) <$> mangleAlt alt <*> mangleExpr ex)
  where
-  mangleAlt (  I.AltData d bs) = I.AltData d <$> mapM lookupBinder bs
-  mangleAlt a@(I.AltLit     _) = return a
-  mangleAlt (  I.AltDefault b) = I.AltDefault <$> lookupBinder b
+  mangleAlt (I.AltData d bs) = I.AltData d <$> mapM mangleAlt bs
+  mangleAlt a@(I.AltLit _) = return a
+  mangleAlt (I.AltBinder b) = I.AltBinder <$> lookupBinder b
 
   lookupBinder :: I.Binder -> Mangle I.Binder
   lookupBinder Nothing = return Nothing
