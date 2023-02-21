5dd5f4fb0c2b5968021c25b62f38dff0b94f4fb1
Emily Sillars <emsillars@gmail.com>
2023-02-21 16:07:21 -0500

    Add par stub (#143)
    * add stub for optimizing par + practice exercises
    
    * update IR.hs

--- src/IR.hs
+++ src/IR.hs
@@ -1,34 +1,41 @@
--- | Intermediate representation (IR) stages of the compiler pipeline.
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
+
+-- | Intermediate representation (IR) stages of the compiler pipeline.
 module IR where
 
-import           Common.Compiler
-import           Common.Default                 ( Default(..) )
-import qualified Front.Ast                     as A
-import qualified IR.IR                         as I
+import Common.Compiler
+import Common.Default (Default (..))
+import qualified Front.Ast as A
+import qualified IR.IR as I
 
-import           IR.ClassInstantiation          ( instProgram )
-import           IR.DConToFunc                  ( dConToFunc )
-import           IR.DesugarPattern              ( desugarPattern )
-import           IR.ExternToCall                ( externToCall )
-import           IR.InsertRefCounting           ( insertRefCounting )
-import           IR.LambdaLift                  ( liftProgramLambdas )
-import           IR.LowerAst                    ( lowerProgram )
-import           IR.SegmentLets                 ( segmentLets )
-import           IR.Types                       ( fromAnnotations
-                                                , typecheckProgram
-                                                )
-import           Control.Monad                  ( (>=>)
-                                                , when
-                                                )
-import           System.Console.GetOpt          ( ArgDescr(..)
-                                                , OptDescr(..)
-                                                )
-import           Text.Show.Pretty
+import Control.Monad (
+  when,
+  (>=>),
+ )
+import IR.ClassInstantiation (instProgram)
+import IR.DConToFunc (dConToFunc)
+import IR.DesugarPattern (desugarPattern)
+import IR.ExternToCall (externToCall)
+import IR.InsertRefCounting (insertRefCounting)
+import IR.LambdaLift (liftProgramLambdas)
+import IR.LowerAst (lowerProgram)
+import IR.OptimizePar (optimizePar)
+import IR.SegmentLets (segmentLets)
+import IR.Types (
+  fromAnnotations,
+  typecheckProgram,
+ )
+import System.Console.GetOpt (
+  ArgDescr (..),
+  OptDescr (..),
+ )
+import Text.Show.Pretty
 
--- | Operation modes for the IR compiler stage.
---
--- By default, 'Continue' completes the pipeline end-to-end.
+
+{- | Operation modes for the IR compiler stage.
+
+ By default, 'Continue' completes the pipeline end-to-end.
+-}
 data Mode
   = Continue
   | DumpIR
@@ -40,42 +47,53 @@ data Mode
   | DumpIRFinal
   deriving (Eq, Show)
 
+
 -- | Compiler options for the IR compiler stage.
-newtype Options = Options { mode :: Mode }
+newtype Options = Options {mode :: Mode}
   deriving (Eq, Show)
 
+
 instance Default Options where
-  def = Options { mode = Continue }
+  def = Options{mode = Continue}
+
 
 -- | CLI options for the IR compiler stage.
 options :: [OptDescr (Options -> Options)]
 options =
-  [ Option ""
-           ["dump-ir"]
-           (NoArg $ setMode DumpIR)
-           "Print the IR immediately after lowering"
-  , Option ""
-           ["dump-ir-annotated"]
-           (NoArg $ setMode DumpIRTyped)
-           "Print the fully-typed IR just before type inference"
-  , Option ""
-           ["dump-ir-typed"]
-           (NoArg $ setMode DumpIRTyped)
-           "Print the fully-typed IR after type inference"
-  , Option ""
-           ["dump-ir-typed-ugly"]
-           (NoArg $ setMode DumpIRTypedShow)
-           "Ugly-Print the fully-typed IR after type inference"
-  , Option ""
-           ["dump-ir-lifted"]
-           (NoArg $ setMode DumpIRLifted)
-           "Print the IR after lambda lifting"
-  , Option ""
-           ["dump-ir-final"]
-           (NoArg $ setMode DumpIRFinal)
-           "Print the last IR representation before code generation"
+  [ Option
+      ""
+      ["dump-ir"]
+      (NoArg $ setMode DumpIR)
+      "Print the IR immediately after lowering"
+  , Option
+      ""
+      ["dump-ir-annotated"]
+      (NoArg $ setMode DumpIRTyped)
+      "Print the fully-typed IR just before type inference"
+  , Option
+      ""
+      ["dump-ir-typed"]
+      (NoArg $ setMode DumpIRTyped)
+      "Print the fully-typed IR after type inference"
+  , Option
+      ""
+      ["dump-ir-typed-ugly"]
+      (NoArg $ setMode DumpIRTypedShow)
+      "Ugly-Print the fully-typed IR after type inference"
+  , Option
+      ""
+      ["dump-ir-lifted"]
+      (NoArg $ setMode DumpIRLifted)
+      "Print the IR after lambda lifting"
+  , Option
+      ""
+      ["dump-ir-final"]
+      (NoArg $ setMode DumpIRFinal)
+      "Print the last IR representation before code generation"
   ]
-  where setMode m o = o { mode = m }
+ where
+  setMode m o = o{mode = m}
+
 
 -- | Lower from AST to IR (with annotations).
 lower :: Options -> A.Program -> Pass (I.Program I.Annotations)
@@ -84,16 +102,19 @@ lower opt p = do
   when (mode opt == DumpIR) $ dump $ fmap fromAnnotations p
   return p
 
--- | Type inference + check against type annotations.
---
--- After this stage, no compiler errors should be thrown.
+
+{- | Type inference + check against type annotations.
+
+ After this stage, no compiler errors should be thrown.
+-}
 typecheck :: Options -> I.Program I.Annotations -> Pass (I.Program I.Type)
 typecheck opt p = do
   when (mode opt == DumpIRAnnotated) $ dump $ fmap fromAnnotations p
   p <- typecheckProgram p
   when (mode opt == DumpIRTyped) $ dump p
-  when (mode opt == DumpIRTypedShow) $ (throwError . Dump . ppShow ) p
+  when (mode opt == DumpIRTypedShow) $ (throwError . Dump . ppShow) p
   return p
+
 
 -- | IR transformations to prepare for codegen.
 transform :: Options -> I.Program I.Type -> Pass (I.Program I.Type)
@@ -103,11 +124,13 @@ transform opt p = do
   p <- segmentLets p
   p <- dConToFunc p
   p <- externToCall p
+  p <- optimizePar p
   p <- liftProgramLambdas p
   when (mode opt == DumpIRLifted) $ dump p
   p <- insertRefCounting p
   when (mode opt == DumpIRFinal) $ dump p
   return p
+
 
 -- | IR compiler stage.
 run :: Options -> A.Program -> Pass (I.Program I.Type)
