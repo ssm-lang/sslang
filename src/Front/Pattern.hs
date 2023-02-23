40ddcf7db459b2cf898e2b78f34a9a83a417a32a
Chris Yoon <cjy2129@columbia.edu>
2023-02-23 18:12:18 -0500

    Remove Pattern.hs in src/Front

--- src/Front/Pattern.hs
+++
@@ -1,9 +0,0 @@
-module Front.Pattern where
-
-import Common.Compiler (Pass)
-import qualified Front.Ast as A
-import qualified Front.Pattern.Anomaly as Anomaly
-
-
-checkAnomaly :: A.Program -> Pass ()
-checkAnomaly = Anomaly.checkProgram
