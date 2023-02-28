877aa3ab1d986e48466e25cef6ae7cff79681816
jar2333 <jar2333@columbia.edu>
2023-02-28 05:41:33 -0500

    added pretty printer module

--- src/IR/Constraint/PrettyPrint.hs
+++ src/IR/Constraint/PrettyPrint.hs
@@ -5,5 +5,9 @@ import           IR.Constraint.Type
 import           Prettyprinter
 import           Prettyprinter.Render.String
 
+import           Common.Compiler
+
+-- Make Constraint an instance of Pretty!!!
+
 printConstraint :: Constraint -> String
 printConstraint = "test"
