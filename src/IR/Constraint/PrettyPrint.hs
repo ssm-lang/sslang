7805339ba630ac1670f88dde28bf4de2ed6fbfef
jar2333 <jar2333@columbia.edu>
2023-02-28 02:18:46 -0500

    added pretty printer module

---
+++ src/IR/Constraint/PrettyPrint.hs
@@ -0,0 +1,9 @@
+module IR.Constraint.PrettyPrint where
+
+import           IR.Constraint.Type
+
+import           Prettyprinter
+import           Prettyprinter.Render.String
+
+printConstraint :: Constraint -> String
+printConstraint = "test"
