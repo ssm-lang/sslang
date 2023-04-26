c8220f09b4c0954a29c8e574904990f5b3232162
xiaoqiangheye <qiangwei@uw.com>
2023-04-26 18:54:31 -0400

    extractImportList

--- src/Front/RewriteImportId.hs
+++ src/Front/RewriteImportId.hs
@@ -1,16 +1,22 @@
-module RewriteImportList where
+module Front.RewriteImportId where
 import qualified Common.Compiler as Compiler
 import Common.Identifiers
 import Data.Bifunctor (first)
 import Data.Generics (Data (..), everywhere, mkT)
 import qualified Front.Ast as A
+import Data.List
 
 
 
 -- | Itereate the AST and first Import ASTs and generates a list of imported information
-extractImportList :: A.Program -> Compiler.Pass A.Program
+extractImportList :: A.Program -> [A.Import]
+extractImportList (A.Program defs) =
+	foldr extractImportListTopDef [] defs
+	where extractImportListTopDef (A.TopImport imp) l = imp : l
+	      extractImportListTopDef _ l = l
 
-
+	
 
 -- | rewrite importids with the full ids based on the imported list
 rewriteImportList :: A.Program -> Compiler.Pass A.Program
+rewriteImportList = error ("unimplemented")
